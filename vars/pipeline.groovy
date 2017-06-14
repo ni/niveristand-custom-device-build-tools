7ef2ad59462006f69d39f92b59778f2b84ac968a
Donovan Buck <donovan.buck@ni.com>
2017-06-14 08:38:39 -0500

    Create pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,4 +1,16 @@
 #!/usr/bin/env groovy
 
 def call(component){
+  echo 'Starting the pipeline build...'
+  
+  node('dcafbuild01'){
+    echo 'Checking out the component repo...'
+    checkout scm: [$class: 'GitSCM', branches: [[name: '*/dynamic-load']], extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'http://github/buckd/commonbuild.git']]]
+    echo 'Loading export.groovy...'
+    load 'export.groovy'
+    stage('Build'){
+      echo 'Starting build...'
+      export()
+    }
+  }
 }
