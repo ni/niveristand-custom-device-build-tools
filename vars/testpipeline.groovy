1e2005f092717badce87a84bae153959688b8e67
Donovan Buck <donovan.buck@ni.com>
2017-06-16 07:45:10 -0500

    adding comments

--- vars/testpipeline.groovy
+++ vars/testpipeline.groovy
@@ -2,36 +2,43 @@
 
 //note: this script assumes that it will be invoked from another script after that script has defined the necessary parameters
 
-//These are: [none] at the moment
+//These are: nodeLabel
 
 //This script further assumes that Jenkins is configured (via the Pipeline Shared Libraries plugin) to implicitly include https://github.com/buckd/commonbuild
 //This script also requires the calling component to include a vars/buildSteps.groovy file that defines a function, build()
 
 def call(nodeLabel){
-  echo 'Starting the pipeline build...'
+  echo 'Starting the testpipeline...'
   
   node(nodeLabel){
     bat 'set'
+    //Make buildSteps accessible to all stages in case we need custom steps for different stages
     def buildSteps
     
-    stage('Clean'){
-      echo 'Cleaning'
+    stage('Initial Clean'){
+      echo 'Cleaning the workspace before building.'
       deleteDir()
     }
+    
     stage('Checkout'){
-      echo 'Attempting to get source from repo...'
+      echo 'Attempting to get source from repo.'
       timeout(time: 5, unit: 'MINUTES'){
         checkout scm
       }
-      echo 'Loading component build steps...'
+      //Load buildSteps here so they can be used by any subsequent stages
+      //Currently only used by the 'Build' stage
+      echo 'Loading component build steps.'
       buildSteps = load 'vars/buildSteps.groovy'
     }
+    
     stage('Build'){
       echo 'Starting build...'
       buildSteps.build()
+      echo 'Build Complete.'
     }
+    
     stage('Cleanup'){
-      echo 'Cleaning up workspace...'
+      echo 'Cleaning up workspace after successful build.'
       deleteDir()
     }
   }
