250ff68532bb3d1102975e8c48e5ee4700bfdb91
Donovan Buck <donovan.buck@ni.com>
2017-06-21 14:33:45 -0500

    code cleanup

--- vars/syncCommonbuild.groovy
+++ vars/syncCommonbuild.groovy
@@ -1,9 +1,10 @@
 def call(commonbuild_dir){
   commonbuild_dir = 'commonbuild'
-  
   bat "mkdir ${commonbuild_dir}"
+  
   dir("${commonbuild_dir}"){
     git url: 'https://github.com/buckd/commonbuild', branch: 'dynamic-load'
   }
+  
   return commonbuild_dir
 }
