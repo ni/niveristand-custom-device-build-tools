57035d0d94ba19f63d8e96cb59f5c261837d1318
Donovan Buck <donovan.buck@ni.com>
2017-06-21 14:11:28 -0500

    Create syncCommonbuild.groovy

---
+++ vars/syncCommonbuild.groovy
@@ -0,0 +1,9 @@
+def call(commonbuild_dir){
+  commonbuild_dir = 'commonbuild'
+  
+  bat "mkdir ${commonbuild_dir)"
+  dir(${commonbuild_dir){
+    git url: 'https://github.com/buckd/commonbuild', branch: 'dynamic-load'
+  }
+  return commonbuild_dir
+}
