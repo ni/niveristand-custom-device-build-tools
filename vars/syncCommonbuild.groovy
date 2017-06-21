6f4bf25c4b6aa653e913da76162a854f3b2d09ed
Donovan Buck <donovan.buck@ni.com>
2017-06-21 14:15:20 -0500

    typo

--- vars/syncCommonbuild.groovy
+++ vars/syncCommonbuild.groovy
@@ -2,7 +2,7 @@ def call(commonbuild_dir){
   commonbuild_dir = 'commonbuild'
   
   bat "mkdir ${commonbuild_dir}"
-  dir("${commonbuild_dir"){
+  dir("${commonbuild_dir}"){
     git url: 'https://github.com/buckd/commonbuild', branch: 'dynamic-load'
   }
   return commonbuild_dir
