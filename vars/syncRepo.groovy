2d1102f3838d7553657501b488e23f43d2a3701c
Donovan Buck <donovan.buck@ni.com>
2017-06-22 09:27:22 -0500

    try tokenize

--- vars/syncRepo.groovy
+++ vars/syncRepo.groovy
@@ -1,5 +1,6 @@
 def call(repo, branch){
   echo "${repo}"
-  syncDir = "${repo}" =~ /\/[a-zA-Z0-9_]+$/
+  //syncDir = "${repo}" =~ /\/[a-zA-Z0-9_]+$/
+  syncDir = repo.tokenize('\/').last()
   echo "${syncDir}"
 }
