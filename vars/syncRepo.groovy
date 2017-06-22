b6471d33b7d2697fd2a01c10f39f0c239e335c34
Donovan Buck <donovan.buck@ni.com>
2017-06-22 09:28:11 -0500

    double quote

--- vars/syncRepo.groovy
+++ vars/syncRepo.groovy
@@ -1,6 +1,6 @@
 def call(repo, branch){
   echo "${repo}"
   //syncDir = "${repo}" =~ /\/[a-zA-Z0-9_]+$/
-  syncDir = repo.tokenize('\/').last()
+  syncDir = repo.tokenize("/").last()
   echo "${syncDir}"
 }
