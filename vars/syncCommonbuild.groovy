51f5cecc67ab784806cc03c8bae9ca92f808660a
Donovan Buck <donovan.buck@ni.com>
2017-06-22 10:24:52 -0500

    actually use syncRepo

--- vars/syncCommonbuild.groovy
+++ vars/syncCommonbuild.groovy
@@ -1,16 +1,5 @@
 def call(branch){
-  commonbuildDir = 'commonbuild'
-  bat "mkdir ${commonbuildDir}"
-  
-  if(!branch){
-    branch = 'master'
-  }
-  
-  dir(commonbuildDir){
-    git url: 'https://github.com/buckd/commonbuild', branch: branch
-  }
-  
-  syncRepo('https://github.com/buckd/commonbuild', 'testbranch')
+  commonbuildDir = syncRepo('https://github.com/buckd/commonbuild', branch)
   
   return commonbuildDir
 }
