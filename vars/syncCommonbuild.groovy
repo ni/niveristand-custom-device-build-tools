8c53225a0834ec6d69d2ef60a90ea9f912d7a0a4
Donovan Buck <donovan.buck@ni.com>
2017-06-22 08:51:55 -0500

    typo

--- vars/syncCommonbuild.groovy
+++ vars/syncCommonbuild.groovy
@@ -6,7 +6,7 @@ def call(branch){
     branch = 'master'
   }
   
-  dir("${commonbuildDir}"){
+  dir(commonbuildDir){
     git url: 'https://github.com/buckd/commonbuild', branch: ${branch}
   }
   
