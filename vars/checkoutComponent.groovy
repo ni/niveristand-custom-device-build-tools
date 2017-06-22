e22e82287259e8f3e9a8a49402b5a8ec3d6751d2
Donovan Buck <donovan.buck@ni.com>
2017-06-22 11:40:36 -0500

    Delete checkoutComponent.groovy

--- vars/checkoutComponent.groovy
+++
@@ -1,4 +0,0 @@
-def call(repository, branch){
-  echo "Checking out branch ${branch} in repository ${repository}."
-  checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${repository}"]]])
-}
