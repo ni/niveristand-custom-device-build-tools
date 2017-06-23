f18ad5137fcc120ac00e579a129cb3619f3a931c
Donovan Buck <donovan.buck@ni.com>
2017-06-23 13:47:42 -0500

    use regular copy

--- vars/copyProjectConfig.groovy
+++ vars/copyProjectConfig.groovy
@@ -2,5 +2,5 @@ def call(projectPath, projectName){
   echo "Copying configuration file to $projectPath"
   configFileName = "$projectName" + ".config"
   echo "$configFileName"
-  bat "xcopy /Y \"$WORKSPACE\\commonbuild\\config\\LabVIEW.exe.config\" \"$WORKSPACE\\$projectPath\\$configFileName\""
+  bat "copy /Y \"$WORKSPACE\\commonbuild\\config\\LabVIEW.exe.config\" \"$WORKSPACE\\$projectPath\\$configFileName\""
 }
