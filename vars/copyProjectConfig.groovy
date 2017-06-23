62ea027c3c4d164c102cea361deb80418edb41fc
Donovan Buck <donovan.buck@ni.com>
2017-06-23 13:36:30 -0500

    concatenate strings

--- vars/copyProjectConfig.groovy
+++ vars/copyProjectConfig.groovy
@@ -1,6 +1,6 @@
 def call(projectPath, projectName){
   echo "Copying configuration file to $projectPath"
-  configFileName = "$projectName\.config"
+  configFileName = "$projectName" + ".config"
   echo "$configFileName"
   bat "xcopy /Y \"$WORKSPACE\\commonbuild\\config\\LabVIEW.exe.config\" \"$projectPath\\$configFileName\""
 }
