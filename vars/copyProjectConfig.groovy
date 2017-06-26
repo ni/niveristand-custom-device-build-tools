c9321ebea4e1a32bc75708b80f6996e511c82eaa
Donovan Buck <donovan.buck@ni.com>
2017-06-26 07:22:38 -0500

    only need one parameter

--- vars/copyProjectConfig.groovy
+++ vars/copyProjectConfig.groovy
@@ -1,5 +1,5 @@
-def call(projectPath, projectName){
-  echo "Copying configuration file for $projectPath\\$projectName"
-  configFileName = "$projectName" + ".config"
-  bat "copy /Y \"$WORKSPACE\\commonbuild\\config\\LabVIEW.exe.config\" \"$WORKSPACE\\$projectPath\\$configFileName\""
+def call(projectPath){
+  echo "Copying configuration file for $projectPath"
+  configFileName = "$projectPath" + ".config"
+  bat "copy /Y \"$WORKSPACE\\commonbuild\\config\\LabVIEW.exe.config\" \"$WORKSPACE\\$configFileName\""
 }
