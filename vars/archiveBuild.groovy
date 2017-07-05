fd7a71488a256ae99d961939f4a4caf9210ab6bd
Donovan Buck <donovan.buck@ni.com>
2017-07-05 15:00:26 -0500

    Create archiveBuild.groovy

---
+++ vars/archiveBuild.groovy
@@ -0,0 +1,10 @@
+def call(){
+  def $exportDir = 'export'
+  def archiveDir = "${buildSteps.ARCHIVE_DIR}\\$exportDir"
+  //don't do this delete with the actual archive
+  //this is for testing purposes only
+  if(fileExists(archiveDir)){
+    bat "rmdir \"$archiveDir\" /s /q"
+  }
+    bat "xcopy \"$exportDir\" \"$archiveDir\" /e /i"
+}
