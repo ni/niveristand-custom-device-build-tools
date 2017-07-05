00d8d28aac519cb33864a2077226239560da32e6
Donovan Buck <donovan.buck@ni.com>
2017-07-05 15:14:27 -0500

    take archiveDir as parameter

--- vars/archiveBuild.groovy
+++ vars/archiveBuild.groovy
@@ -1,9 +1,9 @@
-def call(exportDir){
-    def archiveDir = "${buildSteps.ARCHIVE_DIR}\\$exportDir"
+def call(exportDir, archiveDir){
+  def archiveLocation = "$archiveDir\\$exportDir"
   //don't do this delete with the actual archive
   //this is for testing purposes only
-  if(fileExists(archiveDir)){
-    bat "rmdir \"$archiveDir\" /s /q"
+  if(fileExists(archiveLocation)){
+    bat "rmdir \"$archiveLocation\" /s /q"
   }
-    bat "xcopy \"$exportDir\" \"$archiveDir\" /e /i"
+    bat "xcopy \"$exportDir\" \"$archiveLocation\" /e /i"
 }
