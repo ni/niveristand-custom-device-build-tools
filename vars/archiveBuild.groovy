4d0fdd02cd44dcf723edbaa264e72260fa50374a
Donovan Buck <donovan.buck@ni.com>
2017-07-05 15:23:44 -0500

    cleanup

--- vars/archiveBuild.groovy
+++ vars/archiveBuild.groovy
@@ -1,9 +1,11 @@
 def call(exportDir, archiveDir){
   def archiveLocation = "$archiveDir\\$exportDir"
+  
   //don't do this delete with the actual archive
   //this is for testing purposes only
   if(fileExists(archiveLocation)){
     bat "rmdir \"$archiveLocation\" /s /q"
   }
-    bat "xcopy \"$exportDir\" \"$archiveLocation\" /e /i"
+  
+  bat "xcopy \"$exportDir\" \"$archiveLocation\" /e /i"
 }
