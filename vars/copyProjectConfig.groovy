59dd1cd5ca221ff17696b3424dbf93c2a0e4c36a
Donovan Buck <donovan.buck@ni.com>
2017-07-10 13:06:08 -0500

    add comment

--- vars/copyProjectConfig.groovy
+++ vars/copyProjectConfig.groovy
@@ -1,4 +1,7 @@
-def call(projectPath){
+// In the future, this function should use the lvVersion and modify
+// the config file so the correct version of the VeriStand assemblies
+// is loaded. Right now, we're only building for 2016, so it's ok.
+def call(projectPath, lvVersion){
   echo "Copying configuration file for $projectPath"
   configFileName = "$projectPath" + ".config"
   bat "copy /Y \"$WORKSPACE\\commonbuild\\config\\LabVIEW.exe.config\" \"$WORKSPACE\\$configFileName\""
