10ec486644aaf5b158ab6eb71c639363647120b0
Donovan Buck <donovan.buck@ni.com>
2017-07-11 14:49:20 -0500

    use list instead of array

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -2,9 +2,10 @@ class BuildInformation implements Serializable {
 
   public final String nodeLabel
   public final String sourceVersion
-  public final String[] lvVersions
+  //public final String[] lvVersions
+  public final List<String> lvVersions
   
-  public BuildInformation(String nodeLabel, String sourceVersion, String[] lvVersions) {
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
     this.nodeLabel = nodeLabel
     this.sourceVersion = sourceVersion
     this.lvVersions = lvVersions
