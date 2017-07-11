843fad652b8216d7e718a4713c1b9ef4ab0221c7
Donovan Buck <donovan.buck@ni.com>
2017-07-11 15:14:06 -0500

    cleanup

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -2,7 +2,6 @@ class BuildInformation implements Serializable {
 
   public final String nodeLabel
   public final String sourceVersion
-  //public final String[] lvVersions
   public final List<String> lvVersions
   
   public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
