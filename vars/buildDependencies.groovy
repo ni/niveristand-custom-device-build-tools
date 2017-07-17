0ba4f72a9de85aa964e2e02d05e3e13b6fae991a
Donovan Buck <donovan.buck@ni.com>
2017-07-17 10:43:40 -0500

    build master if branch isn't found

--- vars/buildDependencies.groovy
+++ vars/buildDependencies.groovy
@@ -1,16 +1,24 @@
 import hudson.AbortException
 
 def call(buildInformation) {
+  if(!buildInformation.dependencies) {
+    // no dependencies need to build
+    return
+  }
+  
+  echo "Building dependencies for job ${env.JOB_NAME}."
+  
   buildInformation.dependencies.each{dependency->
     try {
-      build "../$dependency/test"
+      build "../$dependency/${env.BRANCH_NAME}"
     } catch(AbortException e) {
+      // check if there is a job for the current branch name
       if(e.getMessage().startsWith('No item named')) {
         echo e.getMessage()
-        echo 'Trying build again.'
-        build "../$dependency/${env.BRANCH_NAME}"
+        echo "Building branch master instead of ${env.BRANCH_NAME}."
+        build "../$dependency/master"
       } else {
-        echo 'Did not catch correctly. Throwing.'
+        // job was found, but an exception occurred during build
         throw e
       }
     }
