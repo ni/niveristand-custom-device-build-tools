9798a85ef4837a99c879d685c1a1ad098c5156b0
Donovan Buck <donovan.buck@ni.com>
2017-07-17 07:27:36 -0500

    add dependencies

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -3,10 +3,12 @@ class BuildInformation implements Serializable {
   public final String nodeLabel
   public final String sourceVersion
   public final List<String> lvVersions
+  public final List<String> dependencies
   
-  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
     this.nodeLabel = nodeLabel
     this.sourceVersion = sourceVersion
     this.lvVersions = lvVersions
+    this.dependencies = dependencies
   }
 }
