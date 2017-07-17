e93673bb614ea3f1774b35cd94a75ceaaf5be6d5
Donovan Buck <donovan.buck@ni.com>
2017-07-17 15:39:36 -0500

    syncRepo already handles branch check

--- vars/syncCommonbuild.groovy
+++ vars/syncCommonbuild.groovy
@@ -1,13 +1,8 @@
 def call(){
   echo 'Cloning commonbuild steps to workspace.'
   
-  def branch = env['library.nivscommonbuild.version']
-  if(!branch  || branch == null) {
-    branch = 'master'
-  }
-  
   def organization = env.JOB_NAME.tokenize("/")[0]
-  
+  def branch = env['library.nivscommonbuild.version']  
   commonbuildDir = syncRepo("https://github.com/$organization/commonbuild", branch)
   return commonbuildDir
 }
