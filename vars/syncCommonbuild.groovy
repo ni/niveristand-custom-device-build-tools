644c1bc78740decf777f01af9d75fde2db8f5ed7
Donovan Buck <donovan.buck@ni.com>
2017-07-17 12:47:53 -0500

    use env for branch

--- vars/syncCommonbuild.groovy
+++ vars/syncCommonbuild.groovy
@@ -1,5 +1,10 @@
-def call(branch){
+def call(){
   echo 'Cloning commonbuild steps to workspace.'
+  def branch = env.library.nivscommonbuild.version  
+  if(!branch) {
+    branch = 'master'
+  }
+  
   commonbuildDir = syncRepo('https://github.com/buckd/commonbuild', branch)
   return commonbuildDir
 }
