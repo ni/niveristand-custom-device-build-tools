69a6fc8861428ec736505ca13198b7be9938992f
Donovan Buck <donovan.buck@ni.com>
2017-07-24 13:58:25 -0500

    add archive to build information

--- vars/buildDependencies.groovy
+++ vars/buildDependencies.groovy
@@ -8,26 +8,25 @@ def call(buildInformation) {
   
   echo "Building dependencies for job ${env.JOB_NAME}."
   
-  def buildVars
+  def dependencyBuild
   
   buildInformation.dependencies.each{dependency->
     try {
-      buildVars = build "../$dependency/${env.BRANCH_NAME}"
+      dependencyBuild = build "../$dependency/${env.BRANCH_NAME}"
     } catch(AbortException e) {
       // check if there is a job for the current branch name
       if(e.getMessage().startsWith('No item named')) {
         echo e.getMessage()
         echo "Building branch master instead of ${env.BRANCH_NAME}."
-        buildVars = build "../$dependency/master"
+        dependencyBuild = build "../$dependency/master"
       } else {
         // job was found, but an exception occurred during build
         throw e
       }
     }
     
-    echo "Build vars is $buildVars"
-    buildVars.buildVariables.keySet().each{key->
-      echo "$key"
-    }
+    def buildVariables = dependencyBuild.buildVariables
+    def dependencyDir = "${dependency}_DEP_DIR"
+    buildInformation.addDependencyArchive(buildVariables, dependencyDir)
   }
 }
