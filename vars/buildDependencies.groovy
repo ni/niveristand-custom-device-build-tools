75bd5813746723eccbc9f87b1dc1dd9cdd3ce321
Donovan Buck <donovan.buck@ni.com>
2017-07-24 12:12:01 -0500

    try to get return values

--- vars/buildDependencies.groovy
+++ vars/buildDependencies.groovy
@@ -8,19 +8,23 @@ def call(buildInformation) {
   
   echo "Building dependencies for job ${env.JOB_NAME}."
   
+  def buildVars
+  
   buildInformation.dependencies.each{dependency->
     try {
-      build "../$dependency/${env.BRANCH_NAME}"
+      buildVars = build "../$dependency/${env.BRANCH_NAME}"
     } catch(AbortException e) {
       // check if there is a job for the current branch name
       if(e.getMessage().startsWith('No item named')) {
         echo e.getMessage()
         echo "Building branch master instead of ${env.BRANCH_NAME}."
-        build "../$dependency/master"
+        buildVars = build "../$dependency/master"
       } else {
         // job was found, but an exception occurred during build
         throw e
       }
     }
+    
+    echo "Build vars is $buildVars"
   }
 }
