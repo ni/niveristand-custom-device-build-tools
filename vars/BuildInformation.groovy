219b9b6221ed047d63542fa4d15956ec54476e17
Donovan Buck <donovan.buck@ni.com>
2017-07-24 13:58:11 -0500

    add dependency map

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -5,6 +5,8 @@ class BuildInformation implements Serializable {
   public final List<String> lvVersions
   public final List<String> dependencies
   
+  public Map<String, String> depDirs
+  
   public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
     this.nodeLabel = nodeLabel
     this.sourceVersion = sourceVersion
@@ -24,5 +26,11 @@ class BuildInformation implements Serializable {
       Dependencies: $dependencies"""
     
     script.echo printString
+  }
+  
+  public void addDependencyArchive(buildVariables, dependencyDir) {
+    if(buildVariables.containsKey(dependencyDir) {
+      depDirs[dependencyDir] = buildVariables[dependencyDir]
+    }
   }
 }
