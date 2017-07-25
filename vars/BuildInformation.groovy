8e645bc76d84e126c26834cb5bed24d93ecdef30
Donovan Buck <donovan.buck@ni.com>
2017-07-25 10:54:24 -0500

    BuildInformation doesn't need depDirs

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -4,14 +4,12 @@ class BuildInformation implements Serializable {
   public final String sourceVersion
   public final List<String> lvVersions
   public final List<String> dependencies
-  public final Map<String, String> depDirs
   
   public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
     this.nodeLabel = nodeLabel
     this.sourceVersion = sourceVersion
     this.lvVersions = lvVersions
     this.dependencies = dependencies
-    this.depDirs = [:]
   }
   
   public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
@@ -26,11 +24,5 @@ class BuildInformation implements Serializable {
       Dependencies: $dependencies"""
     
     script.echo printString
-  }
-  
-  public void addDependencyArchive(buildVariables, dependencyDir) {
-    if(buildVariables.containsKey(dependencyDir)) {
-      depDirs[dependencyDir] = buildVariables[dependencyDir]
-    }
   }
 }
