8d894c6d0dab6f6b4199d1d717b2ae55075824e2
Donovan Buck <donovan.buck@ni.com>
2017-07-26 10:21:32 -0500

    don't need to load a different builder

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -1,32 +1,19 @@
 class BuildInformation implements Serializable {
-
-  private static final String COMMON_BUILD_CLASS = 'CommonBuilder'
   
   public final String nodeLabel
   public final String sourceVersion
   public final List<String> lvVersions
   public final List<String> dependencies
   
-  private final String builderClass
-  
-  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies, String builderClass) {
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
     this.nodeLabel = nodeLabel
     this.sourceVersion = sourceVersion
     this.lvVersions = lvVersions
     this.dependencies = dependencies
-    this.builderClass = builderClass
-  }
-  
-  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
-    this(nodeLabel, sourceVersion, lvVersions, dependencies, COMMON_BUILD_CLASS)
-  }
-  
-  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, String builderClass) {
-    this(nodeLabel, sourceVersion, lvVersions, [], builderClass)
   }
   
   public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
-    this(nodeLabel, sourceVersion, lvVersions, [], COMMON_BUILD_CLASS)
+    this(nodeLabel, sourceVersion, lvVersions, [])
   }
   
   public void printInformation(script) {
