173070bf7babccdd69b0f10e55eabd0f14970f67
Donovan Buck <donovan.buck@ni.com>
2017-07-26 10:30:45 -0500

    allow build steps location to be passed to info

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -1,19 +1,32 @@
 class BuildInformation implements Serializable {
   
+  private static final String DEFAULT_BUILD_STEPS_LOCATION = 'vars/buildSteps.groovy'
+  
   public final String nodeLabel
   public final String sourceVersion
   public final List<String> lvVersions
   public final List<String> dependencies
   
-  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
+  private final String buildStepsLocation
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies, String buildStepsLocation) {
     this.nodeLabel = nodeLabel
     this.sourceVersion = sourceVersion
     this.lvVersions = lvVersions
     this.dependencies = dependencies
+    this.buildStepsLocation = buildStepsLocation
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
+    this(nodeLabel, sourceVersion, lvVersions, dependencies, DEFAULT_BUILD_STEPS_LOCATION)
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, String buildStepsLocation) {
+    this(nodeLabel, sourceVersion, lvVersions, [], buildStepsLocation)
   }
   
   public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
-    this(nodeLabel, sourceVersion, lvVersions, [])
+    this(nodeLabel, sourceVersion, lvVersions, [], DEFAULT_BUILD_STEPS_LOCATION)
   }
   
   public void printInformation(script) {
@@ -28,7 +41,7 @@ class BuildInformation implements Serializable {
   
   public def createBuilder(script) {
     def builder = new CommonBuilder(script, this)
-    builder.loadBuildSteps()
+    builder.loadBuildSteps(buildStepsLocation)
     return builder
   }
 }
