14b4cc1508829fccbed820601cf47e4d008ae886
Donovan Buck <donovan.buck@ni.com>
2017-07-26 10:14:50 -0500

    add build class to BuildInformation

--- vars/BuildInformation.groovy
+++ vars/BuildInformation.groovy
@@ -1,19 +1,32 @@
 class BuildInformation implements Serializable {
 
+  private static final String COMMON_BUILD_CLASS = 'CommonBuilder'
+  
   public final String nodeLabel
   public final String sourceVersion
   public final List<String> lvVersions
   public final List<String> dependencies
   
-  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
+  private final String builderClass
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies, String builderClass) {
     this.nodeLabel = nodeLabel
     this.sourceVersion = sourceVersion
     this.lvVersions = lvVersions
     this.dependencies = dependencies
+    this.builderClass = builderClass
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
+    this(nodeLabel, sourceVersion, lvVersions, dependencies, COMMON_BUILD_CLASS)
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, String builderClass) {
+    this(nodeLabel, sourceVersion, lvVersions, [], builderClass)
   }
   
   public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
-    this(nodeLabel, sourceVersion, lvVersions, [])
+    this(nodeLabel, sourceVersion, lvVersions, [], COMMON_BUILD_CLASS)
   }
   
   public void printInformation(script) {
@@ -24,5 +37,9 @@ class BuildInformation implements Serializable {
       Dependencies: $dependencies"""
     
     script.echo printString
+  }
+  
+  public def CreateBuilder(script) {
+    return new CommonBuilder(script, this)
   }
 }
