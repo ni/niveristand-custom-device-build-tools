15b5574be049d853617dbdd1f9129cb79c5c2240
Donovan Buck <donovan.buck@ni.com>
2017-09-07 14:03:14 -0500

    Add executor

---
+++ src/ni/vsbuild/BuildExecutor.groovy
@@ -0,0 +1,17 @@
+package ni.vsbuild
+
+class BuildExecutor implements Serializable {
+
+  private final def script
+  private final BuildInformation buildInformation
+
+  BuildExecutor(script, BuildInformation buildInformation) {
+    this.script = script
+    this.buildInformation = buildInformation
+  }
+  
+  void execute() {
+    buildInformation.printInformation(script)
+    ni.vsbuild.nipm.Pipeline.builder(script).buildFullPipeline().execute()
+  }
+}
---
+++ src/ni/vsbuild/BuildInformation.groovy
@@ -0,0 +1,58 @@
+package ni.vsbuild
+
+class BuildInformation implements Serializable {
+  
+  private static final String DEFAULT_BUILD_STEPS_LOCATION = 'vars/buildSteps.groovy'
+  
+  public final String nodeLabel
+  public final String sourceVersion
+  public final List<String> lvVersions
+  public final List<String> dependencies
+  public final BuildType buildType
+  public final String buildStepsLocation
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies, String buildStepsLocation, BuildType buildType) {
+    this.nodeLabel = nodeLabel
+    this.sourceVersion = sourceVersion
+    this.lvVersions = lvVersions
+    this.dependencies = dependencies
+    this.buildStepsLocation = buildStepsLocation
+    this.buildType = buildType
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies, BuildType buildType) {
+    this(nodeLabel, sourceVersion, lvVersions, dependencies, DEFAULT_BUILD_STEPS_LOCATION, buildType)
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, String buildStepsLocation, BuildType buildType) {
+    this(nodeLabel, sourceVersion, lvVersions, [], buildStepsLocation, buildType)
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, List<String> dependencies) {
+    this(nodeLabel, sourceVersion, lvVersions, dependencies, DEFAULT_BUILD_STEPS_LOCATION, BuildType.Groovy)
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, String buildStepsLocation) {
+    this(nodeLabel, sourceVersion, lvVersions, [], buildStepsLocation, BuildType.Groovy)
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions, BuildType buildType) {
+    this(nodeLabel, sourceVersion, lvVersions, [], DEFAULT_BUILD_STEPS_LOCATION, buildType)
+  }
+  
+  public BuildInformation(String nodeLabel, String sourceVersion, List<String> lvVersions) {
+    this(nodeLabel, sourceVersion, lvVersions, [], DEFAULT_BUILD_STEPS_LOCATION, BuildType.Groovy)
+  }
+  
+  public void printInformation(script) {
+    def printString = """Building using the following BuildInformation:
+      Build type: $buildType
+      Node label: $nodeLabel
+      Source version: $sourceVersion
+      LV versions to build: $lvVersions
+      Dependencies: $dependencies
+      Build steps location: $buildStepsLocation"""
+    
+    script.echo printString
+  }
+}
