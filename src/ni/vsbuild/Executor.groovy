b38d62fb1726be85296f90efce3e6b4f7dd50c37
Donovan Buck <donovan.buck@ni.com>
2017-09-08 10:37:21 -0500

    try removing namespace

--- src/ni/vsbuild/Executor.groovy
+++ src/ni/vsbuild/Executor.groovy
@@ -4,6 +4,6 @@ class Executor implements Serializable {
   
   static void execute(script, BuildInformation buildInformation) {
     buildInformation.printInformation(script)
-    ni.vsbuild.nipm.Pipeline.builder(script).buildFullPipeline().execute()
+    nipm.Pipeline.builder(script).buildFullPipeline().execute()
   }
 }
