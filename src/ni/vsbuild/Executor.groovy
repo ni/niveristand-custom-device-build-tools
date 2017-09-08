c56b5d09c2eddd2b3fb971d574b7972983dc2974
Donovan Buck <donovan.buck@ni.com>
2017-09-08 10:36:35 -0500

    make class static

--- src/ni/vsbuild/Executor.groovy
+++ src/ni/vsbuild/Executor.groovy
@@ -1,19 +1,6 @@
 package ni.vsbuild
 
 class Executor implements Serializable {
-
-  private final def script
-  private final BuildInformation buildInformation
-
-  Executor(script, buildInformation) {
-    this.script = script
-    this.buildInformation = buildInformation
-  }
-  
-  void execute() {
-    buildInformation.printInformation(script)
-    ni.vsbuild.nipm.Pipeline.builder(script).buildFullPipeline().execute()
-  }
   
   static void execute(script, BuildInformation buildInformation) {
     buildInformation.printInformation(script)
