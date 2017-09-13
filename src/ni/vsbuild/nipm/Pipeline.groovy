63dd7c6f618aed552e85b86bdf415feacac62490
Donovan Buck <donovan.buck@ni.com>
2017-09-13 14:21:36 -0500

    clean up code

--- src/ni/vsbuild/nipm/Pipeline.groovy
+++ src/ni/vsbuild/nipm/Pipeline.groovy
@@ -5,112 +5,110 @@ import ni.vsbuild.shared.stages.*
 
 class Pipeline implements Serializable {
 
-  def script
-  def stages = []
+   def script
+   def stages = []
+   BuildInformation buildInformation
 
-  static builder(script, BuildInformation buildInformation) {
-    return new Builder(script, buildInformation)
-  }
+   static builder(script, BuildInformation buildInformation) {
+      return new Builder(script, buildInformation)
+   }
 
-  static class Builder implements Serializable {
+   static class Builder implements Serializable {
 
-    def script
-    def stages = []
-    BuildInformation buildInformation
+      def script
+      def stages = []
+      BuildInformation buildInformation
 
-    Builder(def script, BuildInformation buildInformation) {
-      this.script = script
-      this.buildInformation = buildInformation
-    }
+      Builder(def script, BuildInformation buildInformation) {
+         this.script = script
+         this.buildInformation = buildInformation
+      }
 
-    def withInitialCleanStage() {
-      stages << new InitialClean(script)
-    }
+      def withInitialCleanStage() {
+         stages << new InitialClean(script)
+      }
 
-    def withCheckoutStage() {
-      stages << new Checkout(script)
-    }
+      def withCheckoutStage() {
+         stages << new Checkout(script)
+      }
     
-    def withSetupStage() {
-      stages << new Setup(script, buildInformation)
-    }
-    
-    def withUnitTestStage() {
-      stages << new UnitTest(script, buildInformation)
-    }
-    
-    def withCodegenStage() {
-      stages << new Codegen(script, buildInformation)
-    }
-    
-    def withBuildStage() {
-      stages << new Build(script, buildInformation)
-    }
-    
-    def withArchiveStage() {
-      stages << new Archive(script, buildInformation)
-    }
-    
-    def withPackageStage() {
-      stages << new PackageBuild(script, buildInformation)
-    }
-    
-    def withPublishStage() {
-      stages << new Publish(script, buildInformation)
-    }
-
-    def withCleanupStage() {
-      stages << new Cleanup(script)
-    }
-
-    def withTestStages() {
-      withInitialCleanStage()
-      withCheckoutStage()
-      withSetupStage()
-      withUnitTestStage()
-    }
-    
-    def withBuildStages() {
-      withCodegenStage()
-      withBuildStage()
-      
-      if(!buildInformation.officiallySupported) {
-        withArchiveStage()
+      def withSetupStage() {
+         stages << new Setup(script, buildInformation)
       }
       
-      withPackageStage()
-    }
-    
-    def buildPipeline() {
-      withTestStages()
-      withBuildStages()
-      
-      if(script.env['BRANCH_NAME'] == 'master') {
-        withPublishStage()
+      def withUnitTestStage() {
+         stages << new UnitTest(script, buildInformation)
       }
       
-      withCleanupStage()
-      
-      return new Pipeline(this)
-    }
-  }
-
-  private Pipeline(Builder builder) {
-    this.script = builder.script
-    this.stages = builder.stages
-  }
-
-  void execute() {
-
-    script.node('dcaf') {
-      for (Stage stage : stages) {
-        try {
-          stage.execute()
-        } catch (err) {
-          script.currentBuild.result = "FAILURE"
-          script.error "Build failed: ${err.getMessage()}"
-        }
+      def withCodegenStage() {
+         stages << new Codegen(script, buildInformation)
       }
-    }
-  }
+      
+      def withBuildStage() {
+         stages << new Build(script, buildInformation)
+      }
+      
+      def withArchiveStage() {
+         stages << new Archive(script, buildInformation)
+      }
+      
+      def withPackageStage() {
+         stages << new PackageBuild(script, buildInformation)
+      }
+      
+      def withPublishStage() {
+         stages << new Publish(script, buildInformation)
+      }
+      
+      def withCleanupStage() {
+         stages << new Cleanup(script)
+      }
+      
+      def buildPipeline() {
+         withInitialCleanStage()
+         withCheckoutStage()
+         withSetupStage()
+         
+         if(buildInformation.officiallySupported) {
+            withUnitTestStage()
+         }
+         
+         withCodegenStage()
+         withBuildStage()
+         
+         if(!buildInformation.officiallySupported) {
+            withArchiveStage()
+         }
+         
+         withPackageStage()
+         
+         if(script.env['BRANCH_NAME'] == 'master') {
+            withPublishStage()
+         }
+         
+         withCleanupStage()
+         
+         return new Pipeline(this)
+      }
+   }
+
+   private Pipeline(Builder builder) {
+      this.script = builder.script
+      this.stages = builder.stages
+      this.buildInformation = builder.buildInformation
+   }
+
+   void execute() {
+
+      script.node(buildInformation.nodeLabel) {
+         for (Stage stage : stages) {
+            try {
+               stage.execute()
+            } catch (err) {
+               script.currentBuild.result = "FAILURE"
+               script.error "Build failed: ${err.getMessage()}"
+            }
+         }
+      }
+   }
 }
