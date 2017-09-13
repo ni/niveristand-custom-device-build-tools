670d468fd00ca8f79b60a3ae9bc276790098d15a
Donovan Buck <donovan.buck@ni.com>
2017-09-13 14:47:34 -0500

    left over code

--- src/ni/vsbuild/PipelineExecutor.groovy
+++ src/ni/vsbuild/PipelineExecutor.groovy
@@ -6,7 +6,7 @@ class PipelineExecutor implements Serializable {
       buildInformation.printInformation(script)
       
       if(buildInformation.packageType == PackageType.NIPM) {
-         pipeline = nipm.Pipeline.builder(script, buildInformation).buildPipeline().execute()
+         nipm.Pipeline.builder(script, buildInformation).buildPipeline().execute()
       } else {
          script.currentBuild.result = "FAILURE"
          script.error "Build failed: PackageType ${buildInformation.packageType} is not supported."
