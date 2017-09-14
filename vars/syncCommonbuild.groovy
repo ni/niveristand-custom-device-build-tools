c72a588d53ace90032c2ec7c557c1beb62c882ec
Donovan Buck <donovan.buck@ni.com>
2017-09-14 09:11:41 -0500

    library name changed

--- vars/syncCommonbuild.groovy
+++ vars/syncCommonbuild.groovy
@@ -2,7 +2,7 @@ def call(){
   echo 'Cloning commonbuild steps to workspace.'
   
   def organization = getComponentParts()['organization']
-  def branch = env['library.nivscommonbuild.version']  
+  def branch = env['library.vs-common-build.version']
   commonbuildDir = syncRepo("https://github.com/$organization/commonbuild", branch)
   return commonbuildDir
 }
