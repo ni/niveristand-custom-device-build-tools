6b949ffe033ad0eab5fbadd903bb08a17c31158f
Donovan Buck <donovan.buck@ni.com>
2017-09-20 10:56:09 -0500

    typo

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -3,7 +3,7 @@ def call() {
    def currentVersion = lvVersion as Integer
    def previousVersion = currentVersion - 1
    def oldAssemblyVersion = "0.0.0.0-${previousVersion}.9.9.9"
-   def newAssemblyVersion = "${currentVersion}.0.0.0
+   def newAssemblyVersion = "${currentVersion}.0.0.0"
    def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
    echo "$testString"
    def changedString = testString.replaceAll("(oldVersion=\")[^\"]+","\$1$oldAssemblyVersion")
