b5376a7c1bd4426f80ec4ebc8d27d61bcb1e022d
Donovan Buck <donovan.buck@ni.com>
2017-09-20 10:42:50 -0500

    try correct substitution

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -1,6 +1,6 @@
 def call() {
    def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
    echo "$testString"
-   def changedString = testString.replaceAll("(oldVersion=\")[^\"]+","\$1")
+   def changedString = testString.replaceAll("(oldVersion=\")[^\"]+","\$10.0.0.0-2016.9.9.9")
    echo "Changed to $changedString."
 }
