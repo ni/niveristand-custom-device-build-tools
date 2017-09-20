5aabd7633dcd3696d332890ecb335cf1e43f128f
Donovan Buck <donovan.buck@ni.com>
2017-09-20 10:38:51 -0500

    add bracket

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -1,6 +1,6 @@
 def call() {
    def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
    echo "$testString"
-   def changedString = testString.replaceAll("(oldVersion=\")","$1")
+   def changedString = testString.replaceAll("(oldVersion=\")","${1}")
    echo "Changed to $changedString."
 }
