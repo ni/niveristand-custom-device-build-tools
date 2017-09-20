0560052b2152ba5467a608152db139a508529788
Donovan Buck <donovan.buck@ni.com>
2017-09-20 10:30:04 -0500

    replace using lookaround

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -1,6 +1,6 @@
 def call() {
    def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
    echo "$testString"
-   def changedString = testString.replaceAll("(oldVersion=\")","foundIt")
+   def changedString = testString.replaceAll("(oldVersion=\")[^\"]+\"","$13.3.3.3")
    echo "Changed to $changedString."
 }
