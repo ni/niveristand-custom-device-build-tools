618fb7baa1eb5303c802f9c120d758ae108c81cd
Donovan Buck <donovan.buck@ni.com>
2017-09-20 10:57:55 -0500

    clean up

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -7,6 +7,6 @@ def call() {
    def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
    echo "$testString"
    def changedString = testString.replaceAll("(oldVersion=\")[^\"]+","\$1$oldAssemblyVersion")
-   changedString = changedString.replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
+                                 .replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
    echo "Changed to $changedString."
 }
