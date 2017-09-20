75f2520e884801e36e54cd8977cafebd9cb1196e
Donovan Buck <donovan.buck@ni.com>
2017-09-20 11:59:24 -0500

    write file

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -4,9 +4,11 @@ def call() {
    def previousVersion = currentVersion - 1
    def oldAssemblyVersion = "0.0.0.0-${previousVersion}.9.9.9"
    def newAssemblyVersion = "${currentVersion}.0.0.0"
-   def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
+   //def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
+   def testString = readFile "commonbuild/config/LabVIEW.exe.config"
    echo "$testString"
    def changedString = testString.replaceAll("(oldVersion=\")[^\"]+","\$1$oldAssemblyVersion")
                                  .replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
    echo "Changed to $changedString."
+   writeFile file: "mytest.config", text: changedString
 }
