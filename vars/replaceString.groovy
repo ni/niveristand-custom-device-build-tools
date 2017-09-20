5ffc05751a4e382eebe5e4ed4c2856b0f828da1a
Donovan Buck <donovan.buck@ni.com>
2017-09-20 10:55:29 -0500

    do math and cleanup

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -1,10 +1,12 @@
 def call() {
    def lvVersion = '2017'
-   def versionAsInteger = lvVersion as Integer
-   def oldMax = versionAsInteger - 1
+   def currentVersion = lvVersion as Integer
+   def previousVersion = currentVersion - 1
+   def oldAssemblyVersion = "0.0.0.0-${previousVersion}.9.9.9"
+   def newAssemblyVersion = "${currentVersion}.0.0.0
    def testString = "oldVersion=\"0.0.0.0-2015.1.0.0\" newVersion=\"2016.0.0.0\""
    echo "$testString"
-   def changedString = testString.replaceAll("(oldVersion=\")[^\"]+","\$10.0.0.0-${oldMax}.9.9.9")
-   changedString = changedString.replaceAll("(newVersion=\")[^\"]+","\$1${versionAsInteger}.0.0.0")
+   def changedString = testString.replaceAll("(oldVersion=\")[^\"]+","\$1$oldAssemblyVersion")
+   changedString = changedString.replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
    echo "Changed to $changedString."
 }
