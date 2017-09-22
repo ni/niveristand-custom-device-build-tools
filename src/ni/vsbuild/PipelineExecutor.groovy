50a7ace574abc880906523226ad88638f8a5b81a
Donovan Buck <donovan.buck@ni.com>
2017-09-22 15:12:26 -0500

    Revert "remove extra stage"
    This reverts commit d17faa61eb4503af3b66ebbaa4803fc4ce260f69.

--- src/ni/vsbuild/PipelineExecutor.groovy
+++ src/ni/vsbuild/PipelineExecutor.groovy
@@ -22,6 +22,8 @@ class PipelineExecutor implements Serializable {
          }
       }
       
-      script.parallel builders
+      script.stage ('CI Build') {
+         script.parallel builders
+      }
    }
 }
