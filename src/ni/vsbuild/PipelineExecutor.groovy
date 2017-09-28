d5bf496327100a588b8f3f178a7aa9c3adde49b5
Donovan Buck <donovan.buck@ni.com>
2017-09-28 14:38:30 -0500

    missed parameters

--- src/ni/vsbuild/PipelineExecutor.groovy
+++ src/ni/vsbuild/PipelineExecutor.groovy
@@ -6,7 +6,7 @@ class PipelineExecutor implements Serializable {
       buildInformation.printInformation(script)
       
       def builders = [:]
-      def pipeline = PipelineFactory.buildPipeline()
+      def pipeline = PipelineFactory.buildPipeline(script, buildInformation)
       
       for (String version in buildInformation.lvVersions) {
          def lvVersion = version // need to bind the variable before the closure - can't do 'for (lvVersion in lvVersions)
