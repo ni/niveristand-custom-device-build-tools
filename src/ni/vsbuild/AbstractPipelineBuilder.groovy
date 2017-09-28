9ddfad0f1f7e585a4503cccb0a53d0c3cd3654af
Donovan Buck <donovan.buck@ni.com>
2017-09-28 13:54:21 -0500

    missed a file

---
+++ src/ni/vsbuild/AbstractPipelineBuilder.groovy
@@ -0,0 +1,57 @@
+package ni.vsbuild
+
+abstract class AbstractPipelineBuilder implements PipelineBuilder, Serializable {
+
+   protected def script
+   protected BuildInformation buildInformation
+   
+   def prebuildStages = []
+   def buildStages = []
+   
+   AbstractPipelineBuilder(script, BuildInformation buildInformation) {
+      this.script = script
+      this.buildInformation = buildInformation
+   }
+   
+   abstract void build()
+   
+   def withInitialCleanStage() {
+      prebuildStages << new InitialClean(script)
+   }
+
+   def withCheckoutStage() {
+      prebuildStages << new Checkout(script)
+   }
+   
+   def withSetupStage() {
+      buildStages << new Setup(script)
+   }
+   
+   def withUnitTestStage() {
+      buildStages << new UnitTest(script)
+   }
+   
+   def withCodegenStage() {
+      buildStages << new Codegen(script)
+   }
+   
+   def withBuildStage() {
+      buildStages << new Build(script)
+   }
+   
+   def withArchiveStage() {
+      buildStages << new Archive(script)
+   }
+   
+   def withPackageStage() {
+      buildStages << new PackageBuild(script)
+   }
+   
+   def withPublishStage() {
+      buildStages << new Publish(script)
+   }
+   
+   def withCleanupStage() {
+      buildStages << new Cleanup(script)
+   }
+}
