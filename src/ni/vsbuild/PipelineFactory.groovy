c033789c86b2e1f8f81e4671d050bd7036cdfc6a
Donovan Buck <donovan.buck@ni.com>
2017-09-28 14:45:32 -0500

    cleanup factory

--- src/ni/vsbuild/PipelineFactory.groovy
+++ src/ni/vsbuild/PipelineFactory.groovy
@@ -3,15 +3,12 @@ package ni.vsbuild
 class PipelineFactory implements Serializable {
    
    static Pipeline buildPipeline(script, BuildInformation buildInformation) {
-      def pipeline
       
       if(buildInformation.packageType == PackageType.NIPM) {
-         pipeline = nipm.Pipeline.builder(script, buildInformation).buildPipeline()
-      } else {
-         script.currentBuild.result = "FAILURE"
-         script.error "Build failed: PackageType ${buildInformation.packageType} is not supported."
+         return nipm.Pipeline.builder(script, buildInformation).buildPipeline()
       }
       
-      return pipeline
+      script.currentBuild.result = "FAILURE"
+      script.error "Build failed: PackageType ${buildInformation.packageType} is not supported."
    }
 }
