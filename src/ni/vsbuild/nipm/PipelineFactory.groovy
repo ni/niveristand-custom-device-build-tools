e0b01e0940ab13075b40eabc04ef6823e6f20db4
Donovan Buck <donovan.buck@ni.com>
2017-09-28 15:11:47 -0500

    remove import

--- src/ni/vsbuild/nipm/PipelineFactory.groovy
+++ src/ni/vsbuild/nipm/PipelineFactory.groovy
@@ -1,11 +1,10 @@
 package ni.vsbuild.nipm
 
-import ni.vsbuild.Pipeline
 import ni.vsbuild.BuildInformation
 
 class PipelineFactory implements Serializable {
    
-   static def buildPipeline(script, BuildInformation buildInformation) {
+   static ni.vsbuild.Pipeline buildPipeline(script, BuildInformation buildInformation) {
       return Pipeline.builder(script, buildInformation).buildPipeline()
    }
 }
