fd5411ae03887c9999703a042fa899efcaa53ab0
Donovan Buck <donovan.buck@ni.com>
2017-09-28 15:00:17 -0500

    need import

--- src/ni/vsbuild/nipm/PipelineFactory.groovy
+++ src/ni/vsbuild/nipm/PipelineFactory.groovy
@@ -1,5 +1,7 @@
 package ni.vsbuild.nipm
 
+import ni.vsbuild.Pipeline
+
 class PipelineFactory implements Serializable {
    
    static Pipeline buildPipeline(script, BuildInformation buildInformation) {
