ab537f98505e8a76bdc47059aa9960d2cb4d12da
Donovan Buck <donovan.buck@ni.com>
2017-09-29 13:47:27 -0500

    empty instead of null

--- vars/groovyFlow.groovy
+++ vars/groovyFlow.groovy
@@ -1,6 +1,6 @@
 import ni.vsbuild.BuildFlow
 import ni.vsbuild.BuildInformation
 
-def call(List<String> lvVersions, List<String> dependencies = null) {
+def call(List<String> lvVersions, List<String> dependencies = []) {
    return new BuildInformation('veristand', lvVersions, dependencies, BuildFlow.GROOVY)
 }
