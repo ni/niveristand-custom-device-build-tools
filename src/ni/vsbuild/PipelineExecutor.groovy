003f59cf6d96525a6a42e84f2fb74e99ebb9d940
Donovan Buck <donovan.buck@ni.com>
2017-09-29 09:56:35 -0500

    let factory determine execution

--- src/ni/vsbuild/PipelineExecutor.groovy
+++ src/ni/vsbuild/PipelineExecutor.groovy
@@ -4,17 +4,7 @@ class PipelineExecutor implements Serializable {
 
    static void execute(script, BuildInformation buildInformation) {
       buildInformation.printInformation(script)
-      
-      def builders = [:]
-      def pipeline = PipelineFactory.buildPipeline(script, buildInformation)
-      
-      for (String version in buildInformation.lvVersions) {
-         def lvVersion = version // need to bind the variable before the closure - can't do 'for (lvVersion in lvVersions)
-         builders[lvVersion] = {
-            pipeline.execute(lvVersion)
-         }
-      }
-      
-      script.parallel builders
+
+      PipelineFactory.buildPipeline(script, buildInformation).execute()
    }
 }
