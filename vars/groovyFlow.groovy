67982f215e6edf339758dcfdda2b5027f46f5726
Donovan Buck <donovan.buck@ni.com>
2017-09-29 13:46:23 -0500

    default param

--- vars/groovyFlow.groovy
+++ vars/groovyFlow.groovy
@@ -1,6 +1,6 @@
 import ni.vsbuild.BuildFlow
 import ni.vsbuild.BuildInformation
 
-def call(List<String> lvVersions, List<String> dependencies) {
+def call(List<String> lvVersions, List<String> dependencies = null) {
    return new BuildInformation('veristand', lvVersions, dependencies, BuildFlow.GROOVY)
 }
