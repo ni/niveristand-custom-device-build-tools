f4be88db0ac73535851dba4cf80c45fe866f26bc
Donovan Buck <donovan.buck@ni.com>
2017-10-06 12:49:43 -0500

    add version to checkout

--- src/ni/vsbuild/BuildInformation.groovy
+++ src/ni/vsbuild/BuildInformation.groovy
@@ -67,7 +67,7 @@ class BuildInformation implements Serializable {
          executor = new groovy.BuildExecutor(script, this, lvVersion)
       }
       
-      script.stage('Checkout') {
+      script.stage("Checkout_${lvVersion}") {
          script.deleteDir()
          script.echo 'Attempting to get source from repo.'
          script.timeout(time: 5, unit: 'MINUTES'){
