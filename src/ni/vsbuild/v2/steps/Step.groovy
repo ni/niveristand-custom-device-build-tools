e5355b4637f90b54d16d88c2b2b5f66d9a2ce052
Donovan Buck <donovan.buck@ni.com>
2017-11-01 14:25:03 -0500

    extends not implements

--- src/ni/vsbuild/v2/steps/Step.groovy
+++ src/ni/vsbuild/v2/steps/Step.groovy
@@ -2,7 +2,7 @@ package ni.vsbuild.v2.steps
 
 import ni.vsbuild.v2.BuildConfiguration
 
-interface Step implements Serializable {
+interface Step extends Serializable {
 
    execute(BuildConfiguration configuration)
 
