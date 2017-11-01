21298feb0e1258e2fa68f415b9cf986224f12cda
Donovan Buck <donovan.buck@ni.com>
2017-11-01 08:22:05 -0500

    see if pip is accessible

--- src/ni/vsbuild/v2/PipelineExecutor.groovy
+++ src/ni/vsbuild/v2/PipelineExecutor.groovy
@@ -13,6 +13,7 @@ class PipelineExecutor implements Serializable {
          }
          script.stage('setup') {
             script.cloneCommonbuild()
+            script.bat "pip --version"
             script.bat "commonbuild\\scripts\\buildSetup.bat"
          }
          script.stage('read config') {
