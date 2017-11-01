67fcbc13d6dddd35584565ad3afe79aa1f990d92
Donovan Buck <donovan.buck@ni.com>
2017-11-01 15:56:02 -0500

    add all targets build

---
+++ src/ni/vsbuild/v2/steps/build/LvBuildSpecAllTargetsStep.groovy
@@ -0,0 +1,18 @@
+package ni.vsbuild.v2.steps.build
+
+import ni.vsbuild.v2.BuildConfiguration
+
+class LvBuildSpecAllTargetsStep extends LvBuildStep {
+
+   def spec
+   
+   LvBuildSpecAllTargetsStep(script, jsonStep) {
+      super(script, jsonStep)
+      this.spec = jsonStep.getString('build_spec')
+   }
+   
+   void execute(BuildConfiguration configuration) {
+      script.lvBuildSpecAllTargets(resolveProject(configuration), spec, '2017')
+   }
+
+}
