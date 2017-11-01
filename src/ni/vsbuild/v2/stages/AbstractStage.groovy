b7e0009cada88bee5a62032ec6a9fa0e4c877710
Donovan Buck <donovan.buck@ni.com>
2017-11-01 11:57:41 -0500

    print configuration in dummy

--- src/ni/vsbuild/v2/stages/AbstractStage.groovy
+++ src/ni/vsbuild/v2/stages/AbstractStage.groovy
@@ -1,13 +1,17 @@
 package ni.vsbuild.v2.stages
 
+import ni.vsbuild.v2.PipelineConfiguration
+
 abstract class AbstractStage implements Stage {
 
    def stageName
    def script
+   def configuration
 
-   AbstractStage(script, String stageName) {
+   AbstractStage(script, String stageName, PipelineConfiguration configuration) {
       this.script = script
       this.stageName = stageName
+      this.configuration = configuration
    }
 
    void execute() {
--- src/ni/vsbuild/v2/stages/Dummy.groovy
+++ src/ni/vsbuild/v2/stages/Dummy.groovy
@@ -2,11 +2,12 @@ package ni.vsbuild.v2.stages
 
 class Dummy extends AbstractStage {
 
-   Dummy(script) {
-      super(script, 'Dummy')
+   Dummy(script, configuration) {
+      super(script, 'Dummy', configuration)
    }
    
    void executeStage() {
       script.echo "This is a dummy stage for testing."
+      configuration.printInformation()
    }
 }
