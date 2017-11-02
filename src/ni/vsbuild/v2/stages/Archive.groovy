b8fd6511604a9849524e0ae05b985ccdd137e286
Donovan Buck <donovan.buck@ni.com>
2017-11-02 16:07:52 -0500

    fix archive location

--- src/ni/vsbuild/v2/stages/Archive.groovy
+++ src/ni/vsbuild/v2/stages/Archive.groovy
@@ -21,8 +21,8 @@ class Archive extends AbstractStage {
    // Builds a string of the form <archiveLocation>\\export\\<branch>\\<build_number>
    private void buildArchiveDir() {
       archiveLocation = configuration.archive.getString('archive_location') +
-                "\\export\\$lvVersion\\" +
-                "${script.env.BRANCH_NAME}\\Build ${script.currentBuild.number}"
+                "\\export\\${script.env.BRANCH_NAME}\\$lvVersion\\" +
+                "Build ${script.currentBuild.number}"
    }
    
    // Set an env var that points to the archive so dependents can find it
