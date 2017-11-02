4528adf24457aa7ded8ef74ff8a9d88acd99c066
Donovan Buck <donovan.buck@ni.com>
2017-11-02 10:30:40 -0500

    try a different way

--- src/ni/vsbuild/v2/stages/Build.groovy
+++ src/ni/vsbuild/v2/stages/Build.groovy
@@ -26,7 +26,7 @@ class Build extends AbstractStage {
    
    private List<Step> buildAllSteps(String path) {
       List<Step> steps = []
-      def jsonSteps = configuration."$path".getJSONArray('steps')
+      def jsonSteps = configuration.getProperty(path).getJSONArray('steps')
       for (def jsonStep in jsonSteps) {
          Step step = StepFactory.create(script, jsonStep)
          steps.add(step)
