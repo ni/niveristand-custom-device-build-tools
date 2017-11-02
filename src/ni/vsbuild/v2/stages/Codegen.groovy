dc95130a84305e76e6f8c16756b6677736fe46dd
Donovan Buck <donovan.buck@ni.com>
2017-11-02 11:14:48 -0500

    implement codegen stage

--- src/ni/vsbuild/v2/stages/Codegen.groovy
+++ src/ni/vsbuild/v2/stages/Codegen.groovy
@@ -1,12 +1,15 @@
 package ni.vsbuild.v2.stages
 
-class Codegen extends AbstractStage {
+import ni.vsbuild.v2.steps.Step
+import ni.vsbuild.v2.steps.StepFactory
+
+class Codegen extends AbstractStepStage {
 
    Codegen(script, configuration) {
       super(script, 'Codegen', configuration)
    }
    
    void executeStage() {
-      script.echo "codegen is ${configuration.codegen}"
+      executeSteps('codegen')
    }
 }
