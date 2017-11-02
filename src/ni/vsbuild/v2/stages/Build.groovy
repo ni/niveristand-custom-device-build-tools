77454eb91fb48ebc0291e329ebf2590e5a8b042c
Donovan Buck <donovan.buck@ni.com>
2017-11-02 10:51:59 -0500

    more cleanup

--- src/ni/vsbuild/v2/stages/Build.groovy
+++ src/ni/vsbuild/v2/stages/Build.groovy
@@ -10,28 +10,9 @@ class Build extends AbstractStage {
    }
    
    void executeStage() {
-      script.echo "build is ${configuration.build}"
-      //List<Step> steps = []
-      //def jsonSteps = configuration.build.getJSONArray('steps')
-      //for (def jsonStep in jsonSteps) {
-         //Step step = StepFactory.create(script, jsonStep)
-         //steps.add(step)
-      //}
-      
       List<Step> steps = StepFactory.create(script, configuration, 'build')
       for(Step step in steps) {
          step.execute(configuration)
       }
-   }
-   
-   private List<Step> getAllSteps(String property) {
-      List<Step> steps = []
-      def jsonSteps = configuration.getProperty(property).getJSONArray('steps')
-      for (def jsonStep in jsonSteps) {
-         Step step = StepFactory.create(script, jsonStep)
-         steps.add(step)
-      }
-      
-      return steps
    }
 }
