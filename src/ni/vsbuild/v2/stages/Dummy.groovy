d6b78e61b9349ae01822bddd4376b99cd283f606
Donovan Buck <donovan.buck@ni.com>
2017-11-02 12:54:36 -0500

    cleanup

--- src/ni/vsbuild/v2/stages/Dummy.groovy
+++ src/ni/vsbuild/v2/stages/Dummy.groovy
@@ -8,8 +8,5 @@ class Dummy extends AbstractStage {
    
    void executeStage() {
       script.echo "This is a dummy stage for testing."
-      script.echo "${configuration.paths}"
-      def builtDir = configuration.getPath('BUILT_DIR')
-      script.echo "BUILT_DIR is $builtDir"
    }
 }
