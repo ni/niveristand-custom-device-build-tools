add4d6682853979c85efe259c3bba54a9451c78e
Donovan Buck <donovan.buck@ni.com>
2017-11-03 08:06:51 -0500

    pass dependencies

--- src/ni/vsbuild/v2/PipelineExecutor.groovy
+++ src/ni/vsbuild/v2/PipelineExecutor.groovy
@@ -2,10 +2,12 @@ package ni.vsbuild.v2
 
 class PipelineExecutor implements Serializable {
 
-   static void execute(script, List<String> lvVersions) {
-      def pipelineInformation = new PipelineInformation('veristand', lvVersions)
+   static void execute(script, List<String> lvVersions, List<String> dependencies = []) {
+      def pipelineInformation = new PipelineInformation('veristand', lvVersions, dependencies)
+      pipelineInformation.printInformation()
+      
       def pipeline = new Pipeline(script, pipelineInformation)
-      pipeline.execute()
+      pipeline.execute(script)
       
       //def configuration = BuildConfiguration.load(script, jsonFile)
       //configuration.printInformation(script)
