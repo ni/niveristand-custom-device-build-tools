261a1cc949316998d30112f3ac64b9c719752b7b
Donovan Buck <donovan.buck@ni.com>
2017-11-16 10:33:48 -0600

    cleanup

--- src/ni/vsbuild/v2/stages/Build.groovy
+++ src/ni/vsbuild/v2/stages/Build.groovy
@@ -1,8 +1,5 @@
 package ni.vsbuild.v2.stages
 
-import ni.vsbuild.v2.steps.Step
-import ni.vsbuild.v2.steps.StepFactory
-
 class Build extends AbstractStepStage {
 
    Build(script, configuration, lvVersion) {
--- src/ni/vsbuild/v2/stages/Codegen.groovy
+++ src/ni/vsbuild/v2/stages/Codegen.groovy
@@ -1,8 +1,5 @@
 package ni.vsbuild.v2.stages
 
-import ni.vsbuild.v2.steps.Step
-import ni.vsbuild.v2.steps.StepFactory
-
 class Codegen extends AbstractStepStage {
 
    Codegen(script, configuration, lvVersion) {
