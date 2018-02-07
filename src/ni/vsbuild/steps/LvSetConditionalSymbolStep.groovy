f2bdb3cfd3773ebf90596f5789850a617c0f5acd
Donovan Buck <donovan.buck@ni.com>
2018-02-07 14:11:59 -0600

    Improve conditional symbols (#5)
    * add conditional support
    
    Set conditional value based on result of condition in build.toml.

--- src/ni/vsbuild/steps/LvSetConditionalSymbolStep.groovy
+++ src/ni/vsbuild/steps/LvSetConditionalSymbolStep.groovy
@@ -5,18 +5,52 @@ import ni.vsbuild.BuildConfiguration
 class LvSetConditionalSymbolStep extends LvProjectStep {
 
    def symbol
-   def value
+   def trueValue
+   def falseValue
+   def condition
 
    LvSetConditionalSymbolStep(script, mapStep, lvVersion) {
       super(script, mapStep, lvVersion)
       this.symbol = mapStep.get('symbol')
-      this.value = mapStep.get('value')
+      this.trueValue = mapStep.get('true_value')
+      this.falseValue = mapStep.get('false_value')
+      this.condition = mapStep.get('condition')
    }
 
    void executeStep(BuildConfiguration configuration) {
       def resolvedProject = resolveProject(configuration)
       def path = resolvedProject.get('path')
+      
+      def value = evaluateCondition() ? trueValue : falseValue
 
       script.lvSetConditionalSymbol(path, symbol, value, lvVersion)
+   }
+   
+   private def evaluateCondition() {
+      def expressionParts = condition.split()
+      def leftSide = expressionParts[0]
+      def operator = expressionParts[1]
+      def rightSide = expressionParts[2]
+
+      leftSide = this."$leftSide"
+
+      if(operator.equals("<")) {
+         return leftSide < rightSide
+      }
+      if(operator.equals("<=")) {
+         return leftSide <= rightSide
+      }
+      if(operator.equals(">")) {
+         return leftSide > rightSide
+      }
+      if(operator.equals(">=")) {
+         return leftSide >= rightSide
+      }
+      if(operator.equals("==")) {
+         return leftSide == rightSide
+      }
+      if(operator.equals("!=")) {
+         return leftSide != rightSide
+      }
    }
 }
