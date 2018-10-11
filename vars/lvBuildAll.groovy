1d2e7547398bc55031cac35cddfb3a2777baadc7
Donovan Buck <donovan.buck@ni.com>
2018-10-11 15:34:37 -0500

    missed space after comma

--- vars/lvBuildAll.groovy
+++ vars/lvBuildAll.groovy
@@ -1,4 +1,4 @@
-def call(project,lvVersion){
+def call(project, lvVersion){
    echo "Building all build specs in project at $project"
    def logFileName = getLogName(project)
    labviewcli("-OperationName ExecuteAllBuildSpecs -ProjectPath \"$WORKSPACE\\$project\" -LogFilePath \"$WORKSPACE\\lvBuildAll_${logFileName}.log\"", lvVersion)
