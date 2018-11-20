a741a16b787113d8cfae653ea9e7a6c9aec4ec20
Ryan Zoeller <rtzoeller@rtzoeller.com>
2018-11-20 13:46:56 -0600

    Fix relative paths failing to resolve in diffs (#11)
    * Fix relative paths failing to resolve in diffs
    
    The previous implementation used git difftool to automatically run a
    script against the files that had changed. git difftool automatically
    created copies of the files being diffed in a temporary location, but
    did not copy rest of the project, causing relative paths to no longer
    resolve.
    
    This change removes the use of git difftool, and instead exports a copy
    of the repo at the pull request target before invoking the diff
    generator on the changed VIs. This allows relative paths to be resolved
    correctly, leading to nicer diffs.
    
    This change also enables generating diffs for .vit and .vim files.
    
    * Move traceback import to the top level
    
    Previously we were only importing it on error, since this script was called once per file by git difftool. Now that it is only called once, we should import traceback at the top level.
    
    * Use cwd argument to specify working directory of child process
    
    This is cleaner than changing the working directory of the parent process twice.

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -6,10 +6,8 @@ def call(lvVersion, diffingPicRepo, githubDiffToken) {
    def resourcesDir = "${WORKSPACE}\\niveristand-custom-device-build-tools\\resources"
    bat "if exist ${diffDir} rd /s /q ${diffDir}"
    bat "mkdir ${diffDir}"
-   def forwardDiffDir = diffDir.replace("\\", "/")
-   def forwardResourcesDir = resourcesDir.replace("\\", "/")
-   bat "git difftool --no-prompt --trust-exit-code --extcmd=\"python '${forwardResourcesDir}/labview_diff.py' \$LOCAL \$REMOTE ${forwardDiffDir} ${lvVersion}\" origin/master HEAD"
+   bat "python -u \"${resourcesDir}/labview_diff.py\" \"${WORKSPACE}\" \"${diffDir}\" ${lvVersion} --target=origin/master"
    // Silencing echo so as to not print out the token.
-   bat "@python \"${forwardResourcesDir}/github_commenter.py\" --token=\"${githubDiffToken}\" --pic-dir=\"${forwardDiffDir}\" --pull-req=\"${CHANGE_ID}\" --info=\"${JOB_NAME}\" --pic-repo=\"${diffingPicRepo}\""
+   bat "@python -u \"${resourcesDir}/github_commenter.py\" --token=\"${githubDiffToken}\" --pic-dir=\"${diffDir}\" --pull-req=\"${CHANGE_ID}\" --info=\"${JOB_NAME}\" --pic-repo=\"${diffingPicRepo}\""
    lvCloseLabVIEW()
 }
