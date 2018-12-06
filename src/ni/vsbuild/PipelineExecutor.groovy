e074bdb99e6bbc79391b178e30c4c37a525f077d
SimonRPSM <simon.perez.santa.maria@ni.com>
2018-12-06 15:17:17 -0600

    Moving nodeLabel to Jenkinsfile (#12)
    * Moving nodeLabel to JenkinsFile
    
    The nodeLabel is now effectively optional. If there is no nodeLabel passed to the executor, it will use an empty string. On an empty string, it will run it on the node with the correct LabVIEW version.

--- src/ni/vsbuild/PipelineExecutor.groovy
+++ src/ni/vsbuild/PipelineExecutor.groovy
@@ -2,11 +2,14 @@ package ni.vsbuild
 
 class PipelineExecutor implements Serializable {
 
-   static void execute(script, List<String> lvVersions, List<String> dependencies = []) {
-      def pipelineInformation = new PipelineInformation('veristand', lvVersions, dependencies)
+   static void execute(script, String nodeLabel, List<String> lvVersions, List<String> dependencies = []) {
+      def pipelineInformation = new PipelineInformation(nodeLabel, lvVersions, dependencies)
       pipelineInformation.printInformation(script)
-
       def pipeline = new Pipeline(script, pipelineInformation)
       pipeline.execute()
    }
+   
+   static void execute(script, List<String> lvVersions, List<String> dependencies = []) {
+      execute(script, '', lvVersions, dependencies)
+   } 
 }
