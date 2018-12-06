badbf94c1744f544533f4d820b4e3ee4d5b24092
SimonRPSM <simon.perez.santa.maria@ni.com>
2018-12-06 14:38:20 -0600

    Update Pipeline.groovy

--- src/ni/vsbuild/Pipeline.groovy
+++ src/ni/vsbuild/Pipeline.groovy
@@ -93,7 +93,6 @@ class Pipeline implements Serializable {
             nodeLabel = "$nodeLabel && ${pipelineInformation.nodeLabel}"
          }
 
-
          builders[lvVersion] = {
             script.node(nodeLabel) {
                setup(lvVersion)
