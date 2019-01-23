10abdb0451fef779dd5e41fa89c8ce27955560ce
SimonRPSM <simon.perez.santa.maria@ni.com>
2019-01-23 15:08:20 -0600

    Revert "Update StepFactory.groovy"
    This reverts commit 93e45a46a769f7a4be70c4dbd2885e3698f06f09.

--- src/ni/vsbuild/steps/StepFactory.groovy
+++ src/ni/vsbuild/steps/StepFactory.groovy
@@ -37,7 +37,7 @@ class StepFactory implements Serializable {
       }
 
       if(type == 'lvUTF') {
-         return new LvRunViStep(script, mapStep, lvVersion)
+         return new LvUTFStep(script, mapStep, lvVersion)
       }
 
       script.failBuild("Type \'$type\' is invalid for step \'${mapStep.get('name')}\'.")
