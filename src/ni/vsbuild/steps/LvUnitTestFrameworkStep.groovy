f1dbb0b07b60ffddf91300b85251cf9487fa2a3c
SimonRPSM <simon.perez.santa.maria@ni.com>
2019-01-24 15:31:46 -0600

    Removing Project from UTF call

--- src/ni/vsbuild/steps/LvUnitTestFrameworkStep.groovy
+++ src/ni/vsbuild/steps/LvUnitTestFrameworkStep.groovy
@@ -3,7 +3,9 @@ package ni.vsbuild.steps
 import ni.vsbuild.BuildConfiguration
 
 class LvUnitTestFrameworkStep extends LvProjectStep {
+
    def UTF_path
+
    LvUnitTestFrameworkStep(script, mapStep, lvVersion) {
       super(script, mapStep, lvVersion)
       this.UTF_path = mapStep.get('UTF_path')
--- vars/lvUTF.groovy
+++ vars/lvUTF.groovy
@@ -1,4 +1,4 @@
 def call(UTF_path, lvVersion){
    echo "SRPSM: Running UTF."
-   labviewcli("-OperationName RunUnitTests -ProjectPath \"$WORKSPACE\\$project\" -JUnitReportPath \"$WORKSPACE\\UTF_path.log\"", lvVersion)
+   labviewcli("-OperationName RunUnitTests -ProjectPath \"$WORKSPACE\\$UTF_path\" -JUnitReportPath \"$WORKSPACE\\UTF_path.log\"", lvVersion)
 }
