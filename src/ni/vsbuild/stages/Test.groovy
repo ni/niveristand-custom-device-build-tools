65b1f0867a7153cdfb68e3f1d50b6d06f73bbb84
SimonRPSM <simon.perez.santa.maria@ni.com>
2019-01-24 09:39:28 -0600

    Update Test.groovy
    Forgot to put the script.echo after the super call.

--- src/ni/vsbuild/stages/Test.groovy
+++ src/ni/vsbuild/stages/Test.groovy
@@ -3,7 +3,6 @@ package ni.vsbuild.stages
 class Test extends AbstractStepStage {
 
    Test(script, configuration, lvVersion) {
-      script.echo "SRPSM: New test object"
       super(script, 'Test', configuration, lvVersion)
    }
 
