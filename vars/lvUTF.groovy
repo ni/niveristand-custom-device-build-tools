ee86e7555afa2689fa425f50060a000395af0909
SimonRPSM <simon.perez.santa.maria@ni.com>
2019-01-24 15:14:58 -0600

    Update lvUTF.groovy

--- vars/lvUTF.groovy
+++ vars/lvUTF.groovy
@@ -1,3 +1,4 @@
 def call(UTF_path, lvVersion){
    echo "SRPSM: Running UTF."
+   labviewcli("-OperationName RunUnitTests -ProjectPath \"$WORKSPACE\\$project\" -JUnitReportPath \"$WORKSPACE\\UTF_path.log\"", lvVersion)
 }
