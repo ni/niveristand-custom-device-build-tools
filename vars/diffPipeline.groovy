743312c826930087f54a17d6e8d99e7fffc0f302
Ryan Zoeller <rtzoeller@rtzoeller.com>
2019-02-07 13:17:02 -0600

    Allow diffs to timeout with failing pipeline
    Also use guard statement to return early for non-pull requests.

--- vars/diffPipeline.groovy
+++ vars/diffPipeline.groovy
@@ -1,36 +1,56 @@
 #!/usr/bin/env groovy
 
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+import hudson.AbortException
+
 //note: this script assumes that it will be invoked from another script after that script has defined the necessary parameters
 
 //This script further assumes that Jenkins is configured (via the Pipeline Shared Libraries plugin) to implicitly include https://github.com/ni/niveristand-custom-device-build-tools
 
 def call(lvVersion) {
-   if (env.CHANGE_ID) {
-      node(lvVersion) {
-         echo 'Starting build...'
-         stage('Pre-Clean') {
-            deleteDir()
-         }
-         stage('SCM_Checkout') {
-            echo 'Attempting to get source from repo...'
-            timeout(time: 5, unit: 'MINUTES') {
-               checkout scm
-            }
+   // Skip diffing for build which aren't pull-requests.
+   // Only pull-requests will have the change ID set.
+   if (!env.CHANGE_ID) {
+      return
+   }
 
-            timeout(time: 5, unit: 'MINUTES') {
-               cloneBuildTools()
-            }
+   node(lvVersion) {
+      echo 'Starting build...'
+      stage('Pre-Clean') {
+         deleteDir()
+      }
+      stage('SCM_Checkout') {
+         echo 'Attempting to get source from repo...'
+         timeout(time: 5, unit: 'MINUTES') {
+            checkout scm
          }
-         // If this change is a pull request, diff vis.
-         stage('Diff VIs') {
+
+         timeout(time: 5, unit: 'MINUTES') {
+            cloneBuildTools()
+         }
+      }
+      // If this change is a pull request, diff vis.
+      stage('Diff VIs') {
+         try {
             timeout(time: 60, unit: 'MINUTES') {
                lvDiff(lvVersion, env.DIFFING_PIC_REPO, env.GITHUB_DIFF_TOKEN)
                echo 'Diff Succeeded!'
             }
+         } catch (FlowInterruptedException | AbortException e) {
+            // We would expect to get a FlowInterruptedException if the timeout occurs.
+            // However this is only true some of the time - depending on how Jenkins
+            // kills the processes, we may get an AbortException from the killed process.
+            // There is no good common ancestor to catch, so we catch both explicitly.
+
+            // There is currently (2/7/19) not a way to have a stage report
+            // failure but have the pipeline report success. We could mark
+            // the build as unstable, but that causes GitHub report a failure.
+            // We will allow the diff to fail semi-silently since the build itself succeeded.
+            print "Diffing VIs failed due to timeout"
          }
-         stage('Cleanup') {
-            deleteDir()
-         }
+      }
+      stage('Cleanup') {
+         deleteDir()
       }
    }
 }
