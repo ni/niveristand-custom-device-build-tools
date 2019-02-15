cfd4f733f561c6de909f36e74ee763a27175d201
Michael Bilyk <33259264+mbilyk@users.noreply.github.com>
2019-02-15 15:16:24 -0600

    Update cloneBuildTools.groovy

--- vars/cloneBuildTools.groovy
+++ vars/cloneBuildTools.groovy
@@ -2,7 +2,7 @@ def call(){
    echo 'Cloning build tools to workspace.'
    
    //def organization = getComponentParts()['organization']
-   def organization = env."library.vs-build-tools.owner"
+   def organization = library.vs-build-tools.owner
    echo organization
    def branch = env."library.vs-build-tools.version"
    buildToolsDir = cloneRepo("https://github.com/$organization/niveristand-custom-device-build-tools", branch)
