af0ed71e7ded8eee6579b8d31b3ca397530c8560
Michael Bilyk <33259264+mbilyk@users.noreply.github.com>
2019-02-15 15:41:41 -0600

    reverted and added branch echo

--- vars/cloneBuildTools.groovy
+++ vars/cloneBuildTools.groovy
@@ -1,10 +1,9 @@
 def call(){
    echo 'Cloning build tools to workspace.'
    
-   //def organization = getComponentParts()['organization']
-   def organization = env."library.vs-build-tools.github.repoOwner"
-   echo organization
+   def organization = getComponentParts()['organization']
    def branch = env."library.vs-build-tools.version"
+   echo branch
    buildToolsDir = cloneRepo("https://github.com/$organization/niveristand-custom-device-build-tools", branch)
    return buildToolsDir
 }
