96844cfc6d983a172dac7874af2a5f4056b7f17c
Michael Bilyk <33259264+mbilyk@users.noreply.github.com>
2019-02-15 15:54:28 -0600

    Revert Back

--- vars/cloneBuildTools.groovy
+++ vars/cloneBuildTools.groovy
@@ -3,7 +3,6 @@ def call(){
    
    def organization = getComponentParts()['organization']
    def branch = env."library.vs-build-tools.version"
-   echo ${branch}
    buildToolsDir = cloneRepo("https://github.com/$organization/niveristand-custom-device-build-tools", branch)
    return buildToolsDir
 }
