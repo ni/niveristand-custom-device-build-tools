ded709430f30710e47a39d0491e24d167e0261e4
mbilyk <michael.v.bilyk@gmail.com>
2019-02-20 14:57:23 -0600

    Added test code that pulls information from Jenkins

--- vars/cloneBuildTools.groovy
+++ vars/cloneBuildTools.groovy
@@ -3,6 +3,9 @@ def call(){
    
    def organization = getComponentParts()['organization']
    def branch = env."library.vs-build-tools.version"
+   Jenkins.instance.getAllItems(AbstractItem.class).each {
+	println(it.fullName)
+   };
    buildToolsDir = cloneRepo("https://github.com/$organization/niveristand-custom-device-build-tools", branch)
    return buildToolsDir
 }
