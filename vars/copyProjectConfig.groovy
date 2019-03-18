43d03a83d2351e071b69a4c7fa65b71169d8dd24
Donovan Buck <donovan.buck@ni.com>
2019-03-18 09:43:12 -0500

    Simplify assembly redirection

--- vars/copyProjectConfig.groovy
+++ vars/copyProjectConfig.groovy
@@ -3,16 +3,11 @@
 def call(projectPath, lvVersion){
    echo "Copying configuration file for $projectPath"
    configFileName = "$projectPath" + ".config"
-   
-   def currentVersion = lvVersion as int
-   def newAssemblyVersion = "${currentVersion}.0.0.0"
-   
-   def previousVersion = currentVersion - 1
-   def oldAssemblyVersion = "0.0.0.0-${previousVersion}.9.9.9"
-   
+
+   def newAssemblyVersion = "${lvVersion}.0.0.0"
+
    def fileContent = readFile "niveristand-custom-device-build-tools/resources/LabVIEW.exe.config"
-   fileContent = fileContent.replaceAll("(oldVersion=\")[^\"]+","\$1$oldAssemblyVersion")
-      .replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
-   
+   fileContent = fileContent.replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
+
    writeFile file: configFileName, text: fileContent
 }
