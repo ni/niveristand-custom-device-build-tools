6e20bb513bc5d0f325da4faf3f6b1bfa0556ac13
Michael Bilyk <Michael.Bilyk@ni.com>
2019-04-10 13:47:14 -0500

    changed to branch instead of master directory

--- vars/lvVITester.groovy
+++ vars/lvVITester.groovy
@@ -1,5 +1,5 @@
 def call(Test_path, lvVersion){
    echo "Running VITester."
-   masterDirectory = WORKSPACE.substring(0, WORKSPACE.lastIndexOf("\\"))
-   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$Test_path\" -AdditionalOperationDirectory \"$masterDirectory\\niveristand-custom-device-testing-tools\\RunVITester\"", lvVersion)
+   branchDirectory = WORKSPACE.substring(0, WORKSPACE.lastIndexOf("\\"))
+   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$Test_path\" -AdditionalOperationDirectory \"$branchDirectory\\niveristand-custom-device-testing-tools\\RunVITester\"", lvVersion)
 }
