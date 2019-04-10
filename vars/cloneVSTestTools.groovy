90bd318e527c1b0453cfb19d5baba4d2d68d9f1b
Michael Bilyk <Michael.Bilyk@ni.com>
2019-04-10 11:07:47 -0500

    Clone vs testing tools to branch directory for use in testing

--- vars/cloneVSTestTools.groovy
+++ vars/cloneVSTestTools.groovy
@@ -3,6 +3,21 @@ def call(){
 
    def organization = getComponentParts()['organization']
    def branch = env."library.vs-test-tools.version"
-   buildToolsDir = cloneRepo("https://github.com/$organization/niveristand-custom-device-testing-tools", branch)
+
+  repo = "https://github.com/$organization/niveristand-custom-device-testing-tools"
+  echo "Cloning $repo to $branch directory."
+  testToolsDir = "..\\" + repo.tokenize("/").last()
+
+  echo "Over-writing old test tools at $testToolsDir"
+  bat "if exist \"$testToolsDir\" rmdir /S /Q \"$testToolsDir\""
+  bat "mkdir $testToolsDir"
+
+  if(!branch  || branch == null){
+     branch = 'master'
+  }
+
+  dir(testToolsDir){
+     git url: repo, branch: branch
+  }
    return testToolsDir
 }
--- vars/lvVITester.groovy
+++ vars/lvVITester.groovy
@@ -1,4 +1,5 @@
 def call(Test_path, lvVersion){
    echo "Running VITester."
-   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$Test_path\" -AdditionalOperationDirectory \"$WORKSPACE\\niveristand-custom-device-testing-tools\\RunVITester\"", lvVersion)
+   masterDirectory = WORKSPACE.substring(0, WORKSPACE.lastIndexOf("\\"))
+   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$Test_path\" -AdditionalOperationDirectory \"$masterDirectory\\niveristand-custom-device-testing-tools\\RunVITester\"", lvVersion)
 }
