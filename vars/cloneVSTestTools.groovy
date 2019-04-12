104aa9fbc7e5704bc03f25cbe16485b0e6053832
Michael Bilyk <33259264+mbilyk@users.noreply.github.com>
2019-04-12 14:57:11 -0500

    Change indentation to 3 spaces

--- vars/cloneVSTestTools.groovy
+++ vars/cloneVSTestTools.groovy
@@ -4,20 +4,20 @@ def call(){
    def organization = getComponentParts()['organization']
    def branch = env."library.vs-test-tools.version"
 
-  repo = "https://github.com/$organization/niveristand-custom-device-testing-tools"
-  echo "Cloning $repo to $branch directory."
-  testToolsDir = "..\\" + repo.tokenize("/").last()
+   repo = "https://github.com/$organization/niveristand-custom-device-testing-tools"
+   echo "Cloning $repo to $branch directory."
+   testToolsDir = "..\\" + repo.tokenize("/").last()
 
-  echo "Over-writing old test tools at $testToolsDir"
-  bat "if exist \"$testToolsDir\" rmdir /S /Q \"$testToolsDir\""
-  bat "mkdir $testToolsDir"
+   echo "Over-writing old test tools at $testToolsDir"
+   bat "if exist \"$testToolsDir\" rmdir /S /Q \"$testToolsDir\""
+   bat "mkdir $testToolsDir"
 
-  if(!branch  || branch == null){
-     branch = 'master'
-  }
+   if(!branch  || branch == null){
+      branch = 'master'
+   }
 
-  dir(testToolsDir){
-     git url: repo, branch: branch
-  }
+   dir(testToolsDir){
+      git url: repo, branch: branch
+   }
    return testToolsDir
 }
