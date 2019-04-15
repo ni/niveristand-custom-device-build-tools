fe90c4fb803d1344bfaeadc09ca19f02f3e783d6
Michael Bilyk <Michael.Bilyk@ni.com>
2019-04-15 15:08:58 -0500

    Changed toml test path variable to test_path, changed groovy test path variable to testPath

--- vars/lvVITester.groovy
+++ vars/lvVITester.groovy
@@ -1,5 +1,5 @@
-def call(Test_path, lvVersion){
+def call(testPath, lvVersion){
    echo "Running VITester."
    branchDirectory = WORKSPACE.substring(0, WORKSPACE.lastIndexOf("\\"))
-   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$Test_path\" -AdditionalOperationDirectory \"$branchDirectory\\niveristand-custom-device-testing-tools\\RunVITester\"", lvVersion)
+   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$testPath\" -AdditionalOperationDirectory \"$branchDirectory\\niveristand-custom-device-testing-tools\\RunVITester\"", lvVersion)
 }
