01a3ba33bdb98fa47aea74ae33538ac9c3454e55
Michael Bilyk <33259264+mbilyk@users.noreply.github.com>
2019-04-16 10:17:28 -0500

    Changed branchDirectory to baseDirectory

--- vars/lvVITester.groovy
+++ vars/lvVITester.groovy
@@ -1,6 +1,6 @@
 def call(testPath, lvVersion){
    echo "Running VITester."
-   branchDirectory = WORKSPACE.substring(0, WORKSPACE.lastIndexOf("\\"))
+   baseDirectory = WORKSPACE.substring(0, WORKSPACE.lastIndexOf("\\"))
    def logFileName = getLogName(testPath)
-   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$testPath\" -AdditionalOperationDirectory \"$branchDirectory\\niveristand-custom-device-testing-tools\\RunVITester\" -LogFilePath \"$WORKSPACE\\lvVITester_${logFileName}.log\"", lvVersion)
+   labviewcli("-OperationName RunVITester -TestPath \"$WORKSPACE\\$testPath\" -AdditionalOperationDirectory \"$baseDirectory\\niveristand-custom-device-testing-tools\\RunVITester\" -LogFilePath \"$WORKSPACE\\lvVITester_${logFileName}.log\"", lvVersion)
 }
