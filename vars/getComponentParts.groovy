d758b0b0f0d08df2f8b350773cf690be3d8c8866
Donovan Buck <donovan.buck@ni.com>
2019-07-29 15:44:49 -0500

    Fix flakiness with organization

--- vars/getComponentParts.groovy
+++ vars/getComponentParts.groovy
@@ -1,16 +1,10 @@
 def call() {
    def partMap = [:]
    def tokens = env.JOB_NAME.tokenize("/")
-   def tokenCount = tokens.size()
-
-   def index = -1
-   def parts = ['branch', 'repo', 'organization']
-
-   for(i = 0; i < tokenCount; i++) {
-      if(parts[i]) {
-         partMap[parts[i]] = tokens[index]
-      }
-      index--
+   partMap['branch'] = tokens[-1]
+   partMap['repo'] = tokens[-2]
+   if(tokens[-3]) {
+      partMap['organization'] = tokens[-3]
    }
 
    return partMap
