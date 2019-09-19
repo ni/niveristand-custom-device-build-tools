1faa777fc9de713db2023d618f131272a901e750
Ryan Zoeller <ryan.zoeller@ni.com>
2019-09-19 11:21:51 -0500

    Do nothing but return the string

--- src/ni/vsbuild/StringSubstitution.groovy
+++ src/ni/vsbuild/StringSubstitution.groovy
@@ -3,18 +3,6 @@ package ni.vsbuild
 class StringSubstitution implements Serializable {
 
    public static String replaceStrings(text, lvVersion, additionalReplacements = [:]) {
-      def replacements = [
-            "labview_version": lvVersion,
-            "veristand_version": lvVersion,
-            "labview_short_version": "19",
-      ]
-//      replacements = replacements + additionalReplacements
-
-      def updatedText = text
-//      replacements.each { expression, value ->
-//         updatedText = updatedText.replaceAll("\\{$expression\\}", value)
-//      }
-
-      return updatedText
+      return text
    }
 }
