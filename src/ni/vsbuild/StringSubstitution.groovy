d9c4c17fc6c430dce34e81c4972e6bc4caf5ea13
Ryan Zoeller <ryan.zoeller@ni.com>
2019-09-19 09:49:09 -0500

    Debugging code

--- src/ni/vsbuild/StringSubstitution.groovy
+++ src/ni/vsbuild/StringSubstitution.groovy
@@ -3,18 +3,22 @@ package ni.vsbuild
 class StringSubstitution implements Serializable {
 
    public static String replaceStrings(text, lvVersion, additionalReplacements = [:]) {
+      println("Replacing strings")
       def replacements = [
             "labview_version": lvVersion,
             "veristand_version": lvVersion,
             "labview_short_version": lvVersion[-2..-1],
       ]
+      println("Built initial replacements")
       replacements << additionalReplacements
+      println("Added additional replacements")
 
       def updatedText = text
       replacements.each { expression, value ->
          updatedText = updatedText.replaceAll("\\{$expression\\}", value)
       }
 
+      println("Replaced all strings")
       return updatedText
    }
 }
