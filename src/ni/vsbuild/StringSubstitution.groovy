6938a1427899132c4ae2fb0a950ef45b1962fe10
Ryan Zoeller <ryan.zoeller@ni.com>
2019-09-19 12:04:41 -0500

    Roll back file

--- src/ni/vsbuild/StringSubstitution.groovy
+++ src/ni/vsbuild/StringSubstitution.groovy
@@ -3,6 +3,18 @@ package ni.vsbuild
 class StringSubstitution implements Serializable {
 
    public static String replaceStrings(text, lvVersion, additionalReplacements = [:]) {
-      return text
+      def replacements = [
+            "labview_version": lvVersion,
+            "veristand_version": lvVersion,
+            "labview_short_version": lvVersion.substring(lvVersion.length() - 2),
+      ]
+      replacements << additionalReplacements
+
+      def updatedText = text
+      replacements.each { expression, value ->
+         updatedText = updatedText.replaceAll("\\{$expression\\}", value)
+      }
+
+      return updatedText
    }
 }
