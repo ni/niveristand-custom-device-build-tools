8938dfad2c6b57a16f7004c9d225766a861f2ec4
Donovan Buck <donovan.buck@ni.com>
2020-01-07 10:17:27 -0600

    Fix config files not being correctly copied (#83)

--- vars/copyProjectConfig.groovy
+++ vars/copyProjectConfig.groovy
@@ -2,7 +2,7 @@
 // and versions, then replaces the versions based on the lvVersion
 def call(projectPath, lvVersion){
    echo "Copying configuration file for $projectPath"
-   configFileName = "$projectPath" + ".config"
+   def configFileName = "$projectPath" + ".config"
    
    def defaultVersion = ["$lvVersion": "${lvVersion}.0.0.0"]
    def assemblyVersions = readProperties defaults: defaultVersion, file: "niveristand-custom-device-build-tools/resources/assemblyVersions.properties"
