6a2dbeccd16d3b28cc674c6913cf5a5fb6c50378
Donovan Buck <donovan.buck@ni.com>
2020-07-20 21:13:17 -0500

    Remove pipeline result from factory

--- src/ni/vsbuild/notifications/NotificationFactory.groovy
+++ src/ni/vsbuild/notifications/NotificationFactory.groovy
@@ -4,11 +4,11 @@ import ni.vsbuild.PipelineResult
 
 class NotificationFactory implements Serializable {
 
-   static Notification createNotification(script, notificationInfo, pipelineResult) {
+   static Notification createNotification(script, notificationInfo) {
       def type = notificationInfo.get('type')
 
       if(type == 'teams') {
-         return new TeamsNotification(script, notificationInfo, pipelineResult)
+         return new TeamsNotification(script, notificationInfo)
       }
 
       script.failBuild("\'$type\' is an invalid notification type.")
