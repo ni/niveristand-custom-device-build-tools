6add09f267cdc80cb612e53335e51e9135aa3a57
Donovan Buck <donovan.buck@ni.com>
2020-07-20 21:58:24 -0500

    Add color to notification card

--- src/ni/vsbuild/notifications/TeamsNotification.groovy
+++ src/ni/vsbuild/notifications/TeamsNotification.groovy
@@ -2,12 +2,24 @@ package ni.vsbuild.notifications
 
 class TeamsNotification extends AbstractNotification {
 
+   private static final String GREEN = '00FF00'
+   private static final String RED = 'FF0000'
+
    TeamsNotification(script, notificationInfo) {
       super(script, notificationInfo)
    }
 
    void sendNotification(pipelineResult) {
-      def notificationMessage = NotificationMessage.getSimpleMessage(script, pipelineResult)
-      script.office365ConnectorSend webhookUrl: "${script.env.MSTEAMS_HOOK}", message: notificationMessage, status: "${pipelineResult.name()}"
+      def notificationMessage = "Build ${pipelineResult.toString()}."
+      script.office365ConnectorSend webhookUrl: "${script.env.MSTEAMS_HOOK}",
+         message: notificationMessage,
+         status: "${pipelineResult.name()}",
+         color: getNotificationColor(pipelineResult)
+   }
+
+   private String getNotificationColor(pipelineResult) {
+      def successfulResult = (pipelineResult == PipelineResult.SUCCESS || pipelineResult == PipelineResult.FIXED)
+      def notificationColor = successfulResult ? TeamsNotification.GREEN : TeamsNotification.RED
+      return notificationColor
    }
 }
