624907e3d357432ba6719e6099707dc5f5f5884e
Donovan Buck <donovan.buck@ni.com>
2020-07-21 09:04:47 -0500

    Clean up unused code

--- src/ni/vsbuild/notifications/AbstractNotification.groovy
+++ src/ni/vsbuild/notifications/AbstractNotification.groovy
@@ -5,11 +5,9 @@ import ni.vsbuild.PipelineResult
 abstract class AbstractNotification implements Notification {
 
    def script
-   def type
 
    AbstractNotification(script, notificationInfo) {
       this.script = script
-      this.type = notificationInfo.get('type')
    }
 
    void notify(PipelineResult pipelineResult) {
