ecf5c309196d4f2e08d398602a11e5a415376633
Donovan Buck <donovan.buck@ni.com>
2021-02-03 09:38:26 -0600

    Add imports

--- vars/diffPipeline.groovy
+++ vars/diffPipeline.groovy
@@ -2,6 +2,8 @@
 
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 import hudson.AbortException
+import java.nio.file.FileSystemException
+import jenkins.util.io.CompositeIOException
 
 //note: this script assumes that it will be invoked from another script after that script has defined the necessary parameters
 
