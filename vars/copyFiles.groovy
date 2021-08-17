c7ec6d54f3be79c9bb419b6447570b3a13f31941
Donovan Buck <donovan.buck@ni.com>
2021-08-17 08:11:55 -0500

    format copy exclusions

--- vars/copyFiles.groovy
+++ vars/copyFiles.groovy
@@ -7,11 +7,13 @@ def call(sourceDirectory, destinationDirectory, options=[:]) {
    def commandSwitches = "/nfl /ndl /njh /njs /nc /ns /np"
 
    if(options.exclusions) {
-      commandSwitches = "$commandSwitches /xf \"${options.exclusions}\""
+      def filesToExclude = formatFileList(options.exclusions)
+      commandSwitches = "$commandSwitches /xf $filesToExclude"
    }
 
    if(options.directoryExclusions) {
-      commandSwitches = "$commandSwitches /xd \"${options.directoryExclusions}\""
+      def dirsToExclude = formatFileList(options.directoryExclusions)
+      commandSwitches = "$commandSwitches /xd $dirsToExclude"
    }
 
    // If the files argument is not passed, mirror the entire source to destination
