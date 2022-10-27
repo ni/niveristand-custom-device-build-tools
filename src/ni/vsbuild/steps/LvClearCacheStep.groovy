76c5af2f578aeea7fa2a0e1a94727f0e54d70ffd
Donovan Buck <donovan.buck@ni.com>
2022-10-27 09:46:16 -0500

    fix class name

--- src/ni/vsbuild/steps/LvClearCacheStep.groovy
+++ src/ni/vsbuild/steps/LvClearCacheStep.groovy
@@ -2,7 +2,7 @@ package ni.vsbuild.steps
 
 import ni.vsbuild.BuildConfiguration
 
-class LvRunViStep extends LvStep {
+class LvClearCacheStep extends LvStep {
 
    LvRunViStep(script, mapStep, lvVersion) {
       super(script, mapStep, lvVersion)
