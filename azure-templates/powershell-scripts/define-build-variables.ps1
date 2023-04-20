a2264f87be75bc0dedbac488d5a04edfdc7658d9
Paul <paul.power@ni.com>
2023-04-20 15:31:17 -0500

    Move pre-build steps from inline to ps1 Files (#172)
    * move pre-build powershell to ps1 files
    
    * add contents
    
    * add build tools to path
    
    * add azure templates to path
    
    * move checkout ahead of var definition
    
    * clea nup
    
    * clean up try 2
    
    * clean up try 3
    
    * swap to env variables
    
    * moved essentials to its own script
    
    * tab missing
    
    * add default to codegen also
    
    * remove checkout option
    
    * update LabVIEWCLI variable
    
    * updating source branch format
    
    * fix quarterly release typo
    
    * fix control test and repo name
    
    * use env version for ps1 file
    
    * update remaining pre-build variables
    
    * readd build number
    
    * Modify VI to force diff pipeline to run
    
    * spacing
    
    * Revert "Modify VI to force diff pipeline to run"
    
    This reverts commit 18e98a758d32f73dd9e72b81e517e782bc9641fd.
    
    ---------
    
    Co-authored-by: Karl-G1 <karl.grosz@ni.com>

---
+++ azure-templates/powershell-scripts/define-build-variables.ps1
@@ -0,0 +1,98 @@
+param(
+    [string]$lvVersion,
+    [string]$lvBitness,
+    [string]$outputDir,
+    [string]$releaseVersion,
+    [string]$buildTools
+)
+
+# Set file paths for important directories
+Write-Output "Setting $buildTools as the build tools repo..."
+Write-Host "##vso[task.setvariable variable=CD.BuildTools]$buildTools"
+Write-Output "Using `"$PWD`" as the workspace directory..."
+Write-Host "##vso[task.setvariable variable=CD.Workspace]$PWD"
+Write-Output "Using `"$outputDir`" as the build output directory..."
+Write-Host "##vso[task.setvariable variable=CD.BuildOutputPath]$env:CD_REPOSITORY\$outputDir"
+Write-Host "##vso[task.setvariable variable=CD.Nipkg.Path]$env:CD_REPOSITORY\nipkg"
+
+# Set release information
+Write-Output "Determining quarterly release version..."
+$releaseData = "$releaseVersion" -Split "\."
+If ($releaseData[1] -eq "0")
+{
+    $derivedQuarterlyReleaseVersion = "20$($releaseData[0]) Q1"
+}
+If ($releaseData[1] -eq "3")
+{
+$derivedQuarterlyReleaseVersion = "20$($releaseData[0]) Q2"
+}
+If ($releaseData[1] -eq "5")
+{
+    $derivedQuarterlyReleaseVersion = "20$($releaseData[0]) Q3"
+}
+If ($releaseData[1] -eq "8")
+{
+    $derivedQuarterlyReleaseVersion = "20$($releaseData[0]) Q4"
+}
+If (-not($derivedQuarterlyReleaseVersion -match "20.*Q.*"))
+{
+    Write-Error "Release version is not valid.  Should include follow AA.B.C where AA=year, B=quarter(0,3,5,8), and C=patches."
+}
+Write-Output "Configuring release version to `"$releaseVersion`" and quarterly release version to `"$derivedQuarterlyReleaseVersion`"..."
+Write-Host "##vso[task.setvariable variable=CD.Release.Version]$releaseVersion"
+Write-Host "##vso[task.setvariable variable=CD.Release.Quarterly]$derivedQuarterlyReleaseVersion"
+
+# Set LabVIEW version information
+Write-Host "##vso[task.setvariable variable=CD.LabVIEW.Version]$lvVersion"
+Write-Host "##vso[task.setvariable variable=lvVersion]$lvVersion" # Keep legacy version for variables used in packaging
+# When adding a new version of LabVIEW as an option in custom device pipelines, 
+# a new If statement is needed below with relevant variables
+If ("$lvVersion" -eq "2020")
+{
+    Write-Output "Setting variables for LabVIEW 2020..."
+    Write-Host "##vso[task.setvariable variable=CD.LabVIEW.Config]8.0.0.0"
+    Write-Host "##vso[task.setvariable variable=CD.LabVIEW.ShortVersion]20"
+}
+Elseif ("$lvVersion" -eq "2021")
+{
+    Write-Output "Setting variables for LabVIEW 2021..."
+    Write-Host "##vso[task.setvariable variable=CD.LabVIEW.Config]9.0.0.0"
+    Write-Host "##vso[task.setvariable variable=CD.LabVIEW.ShortVersion]21"
+}
+Elseif ("$lvVersion" -eq "2023")
+{
+    Write-Output "Setting variables for LabVIEW 2023..."
+    Write-Host "##vso[task.setvariable variable=CD.LabVIEW.Config]10.0.0.0"
+    Write-Host "##vso[task.setvariable variable=CD.LabVIEW.ShortVersion]23"
+}
+Else
+{
+    Write-Error "Invalid LabVIEW version defined in pipeline.  Use either 2020, 2021, or 2023."
+}
+
+# Set LabVIEW Bitness information
+If ("$lvBitness" -eq "32bit")
+{
+    Write-Output "Setting variables for 32-bit..."
+    $lvPath = "C:\Program Files (x86)\National Instruments\LabVIEW $lvVersion\LabVIEW.exe"
+    Write-Host "##vso[task.setvariable variable=CD.Architecture]x86"
+    Write-Host "##vso[task.setvariable variable=CD.Nipkg.X86Suffix]-x86"
+    Write-Host "##vso[task.setvariable variable=CD.Nipkg.X64Suffix]"
+    Write-Host "##vso[task.setvariable variable=nipkgx64suffix]" # Keep legacy version for variables used in packaging
+}
+Elseif ("$lvBitness" -eq '64bit')
+{
+    Write-Output "Setting variables for 64-bit..."
+    $lvPath = "C:\Program Files\National Instruments\LabVIEW $lvVersion\LabVIEW.exe"
+    Write-Host "##vso[task.setvariable variable=CD.Architecture]x64"
+    Write-Host "##vso[task.setvariable variable=CD.Nipkg.X86Suffix]"
+    Write-Host "##vso[task.setvariable variable=CD.Nipkg.X64Suffix]64"
+    Write-Host "##vso[task.setvariable variable=nipkgx64suffix]64" # Keep legacy version for variables used in packaging
+}
+Else
+{
+    Write-Error "Invalid Bitness defined in pipeline.  Use either 32bit or 64bit."
+}
+
+$operations = "%cd%\$buildTools\lv\operations"
+Write-Host "##vso[task.setvariable variable=CD.LabVIEWCLI]LabVIEWCLI -PortNumber 3363 -LabVIEWPath `"$lvPath`" -AdditionalOperationDirectory `"$operations`" "
---
+++ azure-templates/powershell-scripts/define-essentials.ps1
@@ -0,0 +1,29 @@
+param([string]$archiveDir)
+
+$repoName = "$env:BUILD_REPOSITORY_NAME" -replace ".+\/", ""
+Write-Output "Defining custom device repo name as $repoName..."
+Write-Host "##vso[task.setvariable variable=CD.Repository]$repoName"
+
+If ("$env:BUILD_REASON" -eq "PullRequest")
+{
+    Write-Output "Setting variables for Pull Requests..."
+    $sourceBranch = "$env:SYSTEM_PULLREQUEST_SOURCEBRANCH"
+    Write-Output "Source branch set to `"$sourceBranch`""
+}
+Else
+{
+    Write-Output "Setting variables for CI/Manual builds..."
+    $sourceBranch = "$env:BUILD_SOURCEBRANCH" -replace "refs/heads/", ""
+    Write-Output "Source branch set to `"$sourceBranch`", removed `"refs/heads/`""
+}
+Write-Host "##vso[task.setvariable variable=CD.SourceBranch]$sourceBranch"
+
+If (Test-Path -Path "$archiveDir\NI\export\$sourceBranch\norebuild")
+{
+    Write-Output "norebuild found in `"$sourceBranch`", so skipping this build... Ending build now."
+    Exit 1
+}
+
+$archiveFullPath = "$archiveDir\NI\export\$sourceBranch\"
+Write-Output "Using `"$archiveFullPath`" as archive path..."
+Write-Host "##vso[task.setvariable variable=CD.ArchivePath]$archiveFullPath"
