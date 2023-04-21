fea1a677081366a0100f93d6245dd6f0876f14e9
Paul Power <paul.power@ni.com>
2023-04-21 15:51:07 -0500

    remove lvVersion and clean up variables in powershell

--- azure-templates/powershell-scripts/packaging/stage-directories.ps1
+++ azure-templates/powershell-scripts/packaging/stage-directories.ps1
@@ -1,34 +1,32 @@
 param(
-    [string]$controlFileName,
-    [string]$lvVersion
+    [string]$controlFileName
 )
 # LabVIEW Version needs to be overwritten in packaging since it is not specified during pre-build
-Write-Host "##vso[task.setvariable variable=CD.LabVIEW.Version]$lvVersion)"
 Write-Output "Starting package for $env:CD_LABVIEW_VERSION..."
 
 Write-Output "setting up nipkg directory..."
-If (Test-Path "$(CD.Nipkg.Path)")
+If (Test-Path "$env:CD_NIPKG_PATH")
 {
-  Remove-Item -Path "$(CD.Nipkg.Path)" -Recurse -Force
+  Remove-Item -Path "$env:CD_NIPKG_PATH" -Recurse -Force
 }
-New-Item -Path "$(CD.Nipkg.Path)" -ItemType "Directory"
-New-Item -Path "$(CD.Nipkg.Path)" -Name "control" -ItemType "Directory"
-New-Item -Path "$(CD.Nipkg.Path)" -Name "data" -ItemType "Directory"
-New-Item -Path "$(CD.Nipkg.Path)" -Name "debian-binary" -ItemType "File"
-Set-Content "$(CD.Nipkg.Path)\debian-binary" "2.0\n"
+New-Item -Path "$env:CD_NIPKG_PATH" -ItemType "Directory"
+New-Item -Path "$env:CD_NIPKG_PATH" -Name "control" -ItemType "Directory"
+New-Item -Path "$env:CD_NIPKG_PATH" -Name "data" -ItemType "Directory"
+New-Item -Path "$env:CD_NIPKG_PATH" -Name "debian-binary" -ItemType "File"
+Set-Content "$env:CD_NIPKG_PATH\debian-binary" "2.0\n"
 Copy-Item `
-  -Path "$(CD.Repository)\$controlFileName" `
-  -Destination "$(CD.Nipkg.Path)\control\control"
+  -Path "$env:CD_REPOSITORY\$controlFileName" `
+  -Destination "$env:CD_NIPKG_PATH\control\control"
 
 Write-Output "updating nipkg control version parameters..."
-$contents = (Get-Content -Path "$(CD.Nipkg.Path)\control\control") `
-  -replace "{veristand_version}", "$(CD.LabVIEW.Version)" `
-  -replace "{labview_version}", "$(CD.LabVIEW.Version)" `
-  -replace "{nipkg_version}", "$(CD.Release.Version).$env:CD_BUILDCOUNTER" `
-  -replace "{display_version}", "$(CD.Release.Version)" `
-  -replace "{quarterly_display_version}", "$(CD.Release.Quarterly)" `
-  -replace "{labview_short_version}", "$(CD.LabVIEW.ShortVersion)" `
-  -replace "{pkg_x86_bitness_suffix}", "$(CD.Nipkg.X86Suffix)"
+$contents = (Get-Content -Path "$env:CD_NIPKG_PATH\control\control") `
+  -replace "{veristand_version}", "$env:CD_LABVIEW_VERSION" `
+  -replace "{labview_version}", "$env:CD_LABVIEW_VERSION" `
+  -replace "{nipkg_version}", "$env:CD_RELEASE_VERSION.$env:CD_BUILDCOUNTER" `
+  -replace "{display_version}", "$env:CD_RELEASE_VERSION" `
+  -replace "{quarterly_display_version}", "$env:CD_RELEASE_QUARTERLY" `
+  -replace "{labview_short_version}", "$env:CD_LABVIEW_SHORTVERSION" `
+  -replace "{pkg_x86_bitness_suffix}", "$env:CD_NIPKG_X86SUFFIX"
 Write-Output $contents
-Set-Content -Value $contents -Path "$(CD.Nipkg.Path)\control\control"
+Set-Content -Value $contents -Path "$env:CD_NIPKG_PATH\control\control"
 Write-Host "##vso[task.setvariable variable=CD.InstallerPath]$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\$lvVersion\installer"
