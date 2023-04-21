83df73a3fdb97dc16ccfde81f20c585257fa8ac6
Paul Power <paul.power@ni.com>
2023-04-21 14:54:20 -0500

    reorganize packaging to be separate stage with SEECD support

---
+++ azure-templates/powershell-scripts/packaging/copy-payload.ps1
@@ -0,0 +1,22 @@
+param(
+    [string]$installDir,
+    [string]$payloadDir
+)
+
+New-Item -Path "nipkg\data\$payloadDir" -ItemType "Directory"
+
+$bitnessSpecificPaths = {
+  "$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\$env:CD_LABVIEW_VERSION\x86\$payloadDir",
+  "$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\$env:CD_LABVIEW_VERSION\x64\$payloadDir"
+}
+# some of the payloads may be in x86, and some in x64, so copy from both
+ForEach ($bitnessSpecificPath in $bitnessSpecificPaths)
+{
+  If (-not(Test-Path "$bitnessSpecificPath"))
+  {
+    Copy-Item `
+    -Path "$bitnessSpecificPath\*" `
+    -Destination "nipkg\data\$installDir" `
+    -Recurse
+  }
+}
---
+++ azure-templates/powershell-scripts/packaging/manifest-release-tags.ps1
@@ -0,0 +1,34 @@
+If ( ("$env:CD_SOURCEBRANCH" -match "release") -and (Test-Path $env:CD_INSTALLERPATH) )
+{
+  Write-Output "Adding manifest file to release branch installer directory"
+  $jsonFilePath = Join-Path $env:CD_INSTALLERPATH "manifest.json"
+  $scmObject = [PSCustomObject]@{
+    GIT_BRANCH = "$env:CD_SOURCEBRANCH"
+    GIT_COMMIT = "$env:BUILD_SOURCEVERSION"
+    GIT_URL = "$env:BUILD_REPOSITORY_URI.git"
+  }
+  $outputObject = [PSCustomObject]@{
+    scm = $scmObject
+  }
+  $jsonString = $outputObject | ConvertTo-Json
+  Set-Content -Path $jsonFilePath -Value $jsonString
+}
+Else
+{
+  Write-Output "No release branch packages built; skipping manifest file"
+}
+
+If (-not("$env:CD_SOURCEBRANCH" -match "release"))
+{
+  $cleanedSourceBranch = "$env:CD_SOURCEBRANCH" -replace "\/", "_"
+  Write-Output "Not a release branch, so appending branch name $cleanedSourceBranch to nipkg files..."
+  $packages = Get-ChildItem `
+    -Path "$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\*\installer\*.nipkg"
+  Foreach ($package in $packages)
+  {
+    If (-not("$packageFile" -match "$cleanedSourceBranch.nipkg"))
+    {
+      Rename-Item -Path "$packageFile" -NewName ("$packageFile" -replace ".nipkg", "_$cleanedSourceBranch.nipkg")
+    }
+  }
+}
