ac826bfde37752c52f797b3009cf904728ded20b
Paul Power <paul.power@ni.com>
2023-04-21 19:26:13 -0500

    Use variables to skip double builds

--- azure-templates/powershell-scripts/packaging/copy-payload.ps1
+++ azure-templates/powershell-scripts/packaging/copy-payload.ps1
@@ -2,24 +2,30 @@ param(
     [string]$installDir,
     [string]$payloadDir
 )
-
-If (-not(Test-Path "nipkg\data\$payloadDir"))
+If ("$(Get-Variable env:CD_PACKAGEBUILT_$env:CD_LABVIEW_VERSION -ValueOnly -ErrorAction SilentlyContinue)" -ne "True")
 {
-  New-Item -Path "nipkg\data\$payloadDir" -ItemType "Directory"
-}
-
-$bitnessSpecificPaths = @(
-  "$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\$env:CD_LABVIEW_VERSION\x86\$payloadDir",
-  "$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\$env:CD_LABVIEW_VERSION\x64\$payloadDir"
-)
-# some of the payloads may be in x86, and some in x64, so copy from both
-ForEach ($bitnessSpecificPath in $bitnessSpecificPaths)
-{
-  If (-not(Test-Path "$bitnessSpecificPath"))
+  If (-not(Test-Path "nipkg\data\$payloadDir"))
   {
-    Copy-Item `
-    -Path "$bitnessSpecificPath\*" `
-    -Destination "nipkg\data\$installDir" `
-    -Recurse
+    New-Item -Path "nipkg\data\$payloadDir" -ItemType "Directory"
   }
+
+  $bitnessSpecificPaths = @(
+    "$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\$env:CD_LABVIEW_VERSION\x86\$payloadDir",
+    "$env:CD_ARCHIVEPATH\$env:BUILD_BUILDNUMBER\$env:CD_LABVIEW_VERSION\x64\$payloadDir"
+  )
+  # some of the payloads may be in x86, and some in x64, so copy from both
+  ForEach ($bitnessSpecificPath in $bitnessSpecificPaths)
+  {
+    If (-not(Test-Path "$bitnessSpecificPath"))
+    {
+      Copy-Item `
+      -Path "$bitnessSpecificPath\*" `
+      -Destination "nipkg\data\$installDir" `
+      -Recurse
+    }
+  }
+}
+Else
+{
+  Write-Output "This package was already built, so this step can be skipped."
 }
--- azure-templates/powershell-scripts/packaging/manifest-release-tags.ps1
+++ azure-templates/powershell-scripts/packaging/manifest-release-tags.ps1
@@ -1,34 +1,42 @@
-If ( ("$env:CD_SOURCEBRANCH" -match "release") -and (Test-Path $env:CD_INSTALLERPATH) )
+If ("$(Get-Variable env:CD_PACKAGEBUILT_$env:CD_LABVIEW_VERSION -ValueOnly -ErrorAction SilentlyContinue)" -ne "True")
 {
-  Write-Output "Adding manifest file to release branch installer directory"
-  $jsonFilePath = Join-Path $env:CD_INSTALLERPATH "manifest.json"
-  $scmObject = [PSCustomObject]@{
-    GIT_BRANCH = "$env:CD_SOURCEBRANCH"
-    GIT_COMMIT = "$env:BUILD_SOURCEVERSION"
-    GIT_URL = "$env:BUILD_REPOSITORY_URI.git"
+  If ( ("$env:CD_SOURCEBRANCH" -match "release") -and (Test-Path $env:CD_INSTALLERPATH) )
+  {
+    Write-Output "Adding manifest file to release branch installer directory"
+    $jsonFilePath = Join-Path $env:CD_INSTALLERPATH "manifest.json"
+    $scmObject = [PSCustomObject]@{
+      GIT_BRANCH = "$env:CD_SOURCEBRANCH"
+      GIT_COMMIT = "$env:BUILD_SOURCEVERSION"
+      GIT_URL = "$env:BUILD_REPOSITORY_URI.git"
+    }
+    $outputObject = [PSCustomObject]@{
+      scm = $scmObject
+    }
+    $jsonString = $outputObject | ConvertTo-Json
+    Set-Content -Path $jsonFilePath -Value $jsonString
   }
-  $outputObject = [PSCustomObject]@{
-    scm = $scmObject
+  Else
+  {
+    Write-Output "No release branch packages built; skipping manifest file"
   }
-  $jsonString = $outputObject | ConvertTo-Json
-  Set-Content -Path $jsonFilePath -Value $jsonString
+
+  If (-not("$env:CD_SOURCEBRANCH" -match "release"))
+  {
+    $cleanedSourceBranch = "$env:CD_SOURCEBRANCH" -replace "\/", "_"
+    Write-Output "Not a release branch, so appending branch name $cleanedSourceBranch to nipkg files..."
+    $packages = Get-ChildItem `
+      -Path "$env:CD_INSTALLERPATH\*.nipkg"
+    Foreach ($package in $packages)
+    {
+      If (-not("$packageFile" -match "$cleanedSourceBranch.nipkg"))
+      {
+        Rename-Item -Path "$packageFile" -NewName ("$packageFile" -replace ".nipkg", "_$cleanedSourceBranch.nipkg")
+      }
+    }
+  }
+  Write-Host "##vso[task.setvariable variable=CD.PackageBuilt.$env:CD_LABVIEW_VERSION]True"
 }
 Else
 {
-  Write-Output "No release branch packages built; skipping manifest file"
-}
-
-If (-not("$env:CD_SOURCEBRANCH" -match "release"))
-{
-  $cleanedSourceBranch = "$env:CD_SOURCEBRANCH" -replace "\/", "_"
-  Write-Output "Not a release branch, so appending branch name $cleanedSourceBranch to nipkg files..."
-  $packages = Get-ChildItem `
-    -Path "$env:CD_INSTALLERPATH\*.nipkg"
-  Foreach ($package in $packages)
-  {
-    If (-not("$packageFile" -match "$cleanedSourceBranch.nipkg"))
-    {
-      Rename-Item -Path "$packageFile" -NewName ("$packageFile" -replace ".nipkg", "_$cleanedSourceBranch.nipkg")
-    }
-  }
+  Write-Output "This package was already built, so this step can be skipped."
 }
