2aec45c1cdb8de2f3ec5990b40ab84907f77200b
Karl-G1 <31290917+Karl-G1@users.noreply.github.com>
2023-05-02 10:31:58 -0500

    Limit diffs to 60 VIs to avoid timeout (#177)

--- resources/labview_diff.py
+++ resources/labview_diff.py
@@ -112,6 +112,15 @@ def get_changed_labview_files(target_ref):
 def diff_repo(workspace, output_dir, target_branch, lv_version):
     diffs = get_changed_labview_files(target_branch)
 
+    # Diffing VIs takes ~1 minute per VI and the stage has a one hour timeout.
+    # Limit diff lengths to 60 VIs to avoid using a build node for step that will fail.
+    if sum(1 for _ in diffs) > 60:
+        timeout_message = "More than 60 VIs were modified. Skipping diff pipeline to avoid timeout."
+        print(timeout_message)
+        with open(output_dir + "\\diff_failures.txt", 'w') as file:
+            file.write(timeout_message)
+        return
+
     with export_repo(target_branch) as directory:
         for status, filename in diffs:
             if status == "A":
