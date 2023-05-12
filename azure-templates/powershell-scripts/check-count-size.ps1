00e7b0c267186d490c45fbe22f6bc3395c41f81b
Paul <paul.power@ni.com>
2023-05-12 10:27:33 -0500

    add empty package check and fix nipkg branch issue (#185)

--- azure-templates/powershell-scripts/check-count-size.ps1
+++ azure-templates/powershell-scripts/check-count-size.ps1
@@ -1,5 +1,6 @@
 param(
-  [string]$archiveDir
+  [string]$archiveDir,
+  [boolean]$virtualPackages
 )
 
 If (-not(Test-Path -Path "$archiveDir\NI\export\main"))
@@ -84,14 +85,19 @@ Else
           $compareFileCount = $compareFileSize = 0
         }
 
-        Write-Output "            ________________________________"
-        Write-Output "            | FILE COUNT | FILE SIZE (SUM) |"
-        Write-Output "  main      | $($compareFileCount.ToString().PadLeft(10, " ")) | $($compareFileSize.ToString().PadLeft(15, " ")) |"
-        Write-Output "  thisBuild | $($validateFileCount.ToString().PadLeft(10, " ")) | $($validateFileSize.ToString().PadLeft(15, " ")) |"
+        Write-Output "             ________________________________"
+        Write-Output "             | FILE COUNT | FILE SIZE (SUM) |"
+        Write-Output "  main       | $($compareFileCount.ToString().PadLeft(10, " ")) | $($compareFileSize.ToString().PadLeft(15, " ")) |"
+        Write-Output "  this build | $($validateFileCount.ToString().PadLeft(10, " ")) | $($validateFileSize.ToString().PadLeft(15, " ")) |"
         Write-Output "            ________________________________"
         
         Remove-Item -Recurse -Path "$validateDirectory"
         Remove-Item -Recurse -Path "$compareDirectory"
+        If (($validateFileCount -eq 0) -and (-not($virtualPackages)))
+        {
+          Write-Output "this build's package file `"$($package.Name)`" has no files in it, so validation has failed.  Exiting..."
+          Exit 1
+        }
       }
       Else
       {
