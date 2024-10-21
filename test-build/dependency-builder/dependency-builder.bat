d13e9bbfc751a1d0fb3147250919ddc084b3fb15
Ashutosh Kumar <ashutosh.kumar@ni.com>
2024-10-21 22:25:33 +0530

    fix typo

--- test-build/dependency-builder/dependency-builder.bat
+++ test-build/dependency-builder/dependency-builder.bat
@@ -252,8 +252,6 @@ mkdir "%cd%\dependency-test-2\ni\export\main\12345678.3\2024\x64\Linux_x64"
 copy "output\Windows\DependencyLibrary2.lvlibp" "%cd%\dependency-test-2\ni\export\main\12345678.2\2024\x64\Windows"
 copy "output\Linux_x64\DependencyLibrary2.lvlibp" "%cd%\dependency-test-2\ni\export\main\12345678.2\2024\x64\Linux_x64"
 
---->
-
 echo "Building in LabVIEW 2025 32-bit..."
 LabVIEWCLI ^
   -PortNumber 3363 ^
